a028ec028ba36b487963ae2081fe31d8ffc07fa9
Borna Bešić <besic.borna@gmail.com>
2018-07-23 12:20:45 +0200

    Select (delete later) entries based on protein_id

--- sql/reduce_database.sql
+++ sql/reduce_database.sql
@@ -1,3 +1,5 @@
+/* Views */
+
 CREATE OR REPLACE VIEW human_mouse_proteins AS
 	SELECT *
 	FROM items.proteins AS proteins
@@ -29,26 +31,109 @@ CREATE OR REPLACE VIEW human_mouse_genes_proteins AS
 	);
 
 /*
+
+Delete entries from the following tables based on protein_id (from human_mouse_proteins):
+- proteins_names
+- proteins_linkouts
+- proteins_smartlinkouts
+- proteins_sequences
+- hierarchical_ogs_proteins
+- proteins_hierarchical_ogs
+- proteins_orthgroups
+- proteins_imagematches
+- runs_genes_proteins
+- genes_proteins
+
+*/
+
+SELECT protein_id
+FROM items.proteins_names AS proteins_names
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE proteins_names.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.proteins_linkouts AS proteins_linkouts
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE proteins_linkouts.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.proteins_smartlinkouts AS proteins_smartlinkouts
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE proteins_smartlinkouts.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.proteins_sequences AS proteins_sequences
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE proteins_sequences.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.hierarchical_ogs_proteins AS hierarchical_ogs_proteins
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE hierarchical_ogs_proteins.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.proteins_hierarchical_ogs AS proteins_hierarchical_ogs
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE proteins_hierarchical_ogs.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.proteins_orthgroups AS proteins_orthgroups
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE proteins_orthgroups.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.proteins_imagematches AS proteins_imagematches
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE proteins_imagematches.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.runs_genes_proteins AS runs_genes_proteins
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE runs_genes_proteins.protein_id = human_mouse_proteins.protein_id
+);
+
+SELECT protein_id
+FROM items.genes_proteins AS genes_proteins
+WHERE NOT EXISTS (
+	SELECT 1
+	FROM human_mouse_proteins
+	WHERE genes_proteins.protein_id = human_mouse_proteins.protein_id
+);
+
+/*
+
 Delete entries from the following tables based on species_id:
 - species
 - species_nodes
 - species_to_levels
 - orthgroups_species
 - runs
-
-Delete entries from the following tables based on protein_id (from human_mouse_proteins):
-- proteins_names
-- proteins_linkouts
-- proteins_smartlinkouts
-- proteins_names
-- proteins_sequences
-- hierarchical_ogs_proteins
-- proteins_hierarchical_ogs
-- proteins_orthgroups
-- proteins_imagematches
-
-- runs_genes_proteins
-- genes_proteins
 
 Delete entries from the following tables based on species_id (from human_mouse_runs):
 - runs_orthgroups
@@ -61,13 +146,3 @@ Delete entries from the following tables based on gene_id (from human_mouse_gene
 - genes
 
 */
-
-
--- DELETE
-SELECT protein_id
-FROM items.proteins_smartlinkouts AS smartlinkouts
-WHERE NOT EXISTS (
-	SELECT 1
-	FROM human_mouse_proteins
-	WHERE smartlinkouts.protein_id = human_mouse_proteins.protein_id
-);
