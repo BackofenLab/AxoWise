67f35640180b075056c3cbf4f5d8870d27622b7d
Borna Bešić <besic.borna@gmail.com>
2018-08-03 12:04:27 +0200

    Update test_get_species_id

--- test/test_sql_queries.py
+++ test/test_sql_queries.py
@@ -1,22 +1,18 @@
 import unittest
-from unittest import mock
-import psycopg2
+import database
 
 import sql_queries as SQL
 
 class TestSQLQueries(unittest.TestCase):
 
-    @mock.patch("psycopg2.connect")
-    def test_get_species_id(self, connect):
-        assert connect is psycopg2.connect
+    def test_get_species_id(self):
 
-        connect.return_value.cursor.return_value.fetchone.return_value = (0,)
-        postgres_connection = connect()
-        species_id = SQL.get_species_id(postgres_connection, "Homo sapiens")
+        postgres_connection, _ = database.connect("test/credentials.test.json")
+        species_id = SQL.get_species_id(postgres_connection, "Mus musculus")
     
-        assert species_id == 0
+        assert species_id == 10090
 
-        query, params = SQL.QueryBuilder.species_id_query("Homo sapiens")
+        query, params = SQL.QueryBuilder.species_id_query("Mus musculus")
         connect.return_value.cursor.return_value.execute.assert_called_once_with(query, params)
 
 if __name__ == "__main__":
