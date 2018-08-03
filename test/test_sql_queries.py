8e1f57e10aadc2d8ea40bc42180faab55e5f2511
Borna Bešić <besic.borna@gmail.com>
2018-08-03 12:11:27 +0200

    Use assertEqual in test_get_species_id

--- test/test_sql_queries.py
+++ test/test_sql_queries.py
@@ -10,10 +10,8 @@ class TestSQLQueries(unittest.TestCase):
         postgres_connection, _ = database.connect("test/credentials.test.json")
         species_id = SQL.get_species_id(postgres_connection, "Mus musculus")
     
-        assert species_id == 10090
+        self.assertEqual(species_id, 10090)
 
-        query, params = SQL.QueryBuilder.species_id_query("Mus musculus")
-        connect.return_value.cursor.return_value.execute.assert_called_once_with(query, params)
 
 if __name__ == "__main__":
     unittest.main()
