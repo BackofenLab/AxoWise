fc14482ada76dd0476c52f47a276c8a1e2b48325
Borna Bešić <besic.borna@gmail.com>
2018-09-04 20:07:53 +0200

    Implement all KEGG -> STRING steps

--- KEGG/api.py
+++ KEGG/api.py
@@ -7,6 +7,7 @@ _organisms_endpoint = "http://rest.kegg.jp/list/organism"
 # Template strings for constructing the API endpoints
 _pathway_template = Template("http://rest.kegg.jp/list/pathway/${organism_id}")
 _get_template = Template("http://rest.kegg.jp/get/${entries}${kgml}")
+_map_id_template = Template("https://string-db.org/api/${format}/get_string_ids?identifiers=${identifiers}&caller_identity=cgdb")
 
 def organisms():
     organisms_file = get(_organisms_endpoint)
@@ -24,3 +25,11 @@ def pathway(pathway_id, kgml = False):
     )
     pathway_file = get(endpoint)
     return pathway_file
+
+def map_identifiers_to_STRING(identifiers):
+    endpoint = _map_id_template.substitute(
+        format = "tsv",
+        identifiers = "%0d".join(identifiers)
+    )
+    identifiers_file = get(endpoint)
+    return identifiers_file
--- KEGG/download.py
+++ KEGG/download.py
@@ -1,10 +1,11 @@
 
 import os
 import api
+import parse
 
 import sys
 sys.path.append("..")
-import utils
+from utils import read_table
 
 # Directory for saving the data
 DATA_DIR = "data"
@@ -21,14 +22,42 @@ with open(organisms_path, mode = "w", encoding = "utf-8") as organisms_file:
 
 # Pathways
 print("Downloading pathways...")
-for id1, id2, organism_name, taxonomy in utils.read_table(organisms, (str, str, str, str), "\t"):
+kegg2external_file = open(os.path.join(DATA_DIR, "kegg2external.tsv"), mode = "w", encoding = "utf-8")
+for id1, id2, organism_name, taxonomy in read_table(organisms, (str, str, str, str), "\t"):
     print(organism_name)
     pathways = api.pathways(id2)
 
-    for pathway_id, pathway_name in utils.read_table(pathways, (str, str), "\t"):
-        print(pathway_name)
-        pathway = api.pathway(pathway_id, kgml = False)
+    # Get the pathway
+    for pathway_id, pathway_name in read_table(pathways, (str, str), "\t"):
+        pathway = api.pathway(pathway_id, kgml = True)
 
         pathway_path = os.path.join(PATHWAYS_DIR, pathway_id)
         with open(pathway_path, mode = "w", encoding = "utf-8") as pathway_file:
             pathway_file.write(pathway)
+
+        # Parse KGML
+        pathway_title, gene_ids = parse.parse_KGML(pathway)
+        print(pathway_title)
+        print("Number of genes:", len(gene_ids))
+
+        # Map KEGG identifiers to STRING external identifiers        
+        mapped_identifiers = api.map_identifiers_to_STRING(gene_ids)
+
+        kegg2external = dict()
+        for idx, external_id, species_id, species_name, preferred_name, annotation in read_table(
+            mapped_identifiers,
+            (int, str, int, str, str, str),
+            delimiter = "\t"
+        ):
+            assert gene_ids[idx] not in kegg2external # Make sure the mapping is unique
+            kegg2external[gene_ids[idx]] = external_id
+        
+        num_not_mapped = len(gene_ids) - len(kegg2external)
+        if num_not_mapped > 0:
+            print("{} gene(s) could not be mapped to STRING external ID!".format(num_not_mapped))
+
+        # Save the mappings
+        for mapping in kegg2external.items():
+            kegg2external_file.write("{}\t{}\n".format(*mapping))
+
+kegg2external_file.close()
