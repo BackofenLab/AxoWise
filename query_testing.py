60b6d6d79d526057ab9be407b3953475d4d1b9e8
Borna Bešić <besic.borna@gmail.com>
2018-10-29 10:03:05 +0100

    Update query_testing.py

--- query_testing.py
+++ query_testing.py
@@ -30,25 +30,35 @@ def main():
 
     # Connect to the databases
     postgres_connection, neo4j_graph = database.connect(credentials_path = args.credentials)
-    print(type(neo4j_graph))
     postgres_connection.close()
 
     query1 = """
-        MATCH (pathway:Pathway)<-[:IN]-(p1:Protein {preferred_name: "CCR5"})-[p1p2:ASSOCIATION]->(p2:Protein)-[:IN]->(pathway)
-        RETURN p1, p2, p1p2, pathway
+        MATCH (pathway:Pathway)<-[:IN]-(p1:Protein {name: "CCR5"})-[p1p2:ASSOCIATION]->(p2:Protein)-[:IN]->(pathway)
+        RETURN p1, p2, p1p2, COLLECT(pathway) AS pathways
     """
 
     query2 = """
         MATCH (pathway:Pathway { name: "Cytokine-cytokine receptor interaction" })<-[:IN]-(other)
         WHERE NOT other:Protein
-        RETURN pathway, other
+        RETURN pathway, COLLECT(other) AS compounds_diseases_drugs
+    """
+
+    query3 = """
+        MATCH (class:Class)<-[:IN*]-(pathway:Pathway { name: "Cytokine-cytokine receptor interaction" })<-[:IN]-(other)
+        WHERE NOT other:Protein
+        RETURN pathway, COLLECT(other) AS compounds_diseases_drugs, COLLECT(DISTINCT class) AS classes
     """
 
     print("[Protein - Protein - Pathway query]")
     num_rows, elapsed_time = time_query(neo4j_graph, query1)
     print(num_rows, "row(s) returned in", elapsed_time, "second(s)")
+
     print("[Pathway - Compound - Drug - Disease query]")
     num_rows, elapsed_time = time_query(neo4j_graph, query2)
+    print(num_rows, "row(s) returned in", elapsed_time, "second(s)")
+
+    print("[Classes - Pathway - Compound - Drug - Disease query]")
+    num_rows, elapsed_time = time_query(neo4j_graph, query3)
     print(num_rows, "row(s) returned in", elapsed_time, "second(s)")
 
 if __name__ == "__main__":
