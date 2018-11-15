5187e6565f1525131eb84fd4fe71857354f3954a
Borna Bešić <besic.borna@gmail.com>
2018-11-15 09:06:52 +0100

    Update Cypher tests

--- test/test_cypher_queries.py
+++ test/test_cypher_queries.py
@@ -12,46 +12,53 @@ class TestCypherQueries(unittest.TestCase):
 
         result = Cypher.search_protein(neo4j_graph, "ccr5")
 
+        num_entries = 0
+        has_pathway = False
+        has_ccl5_other = False
+        has_il10ra_other = False
         for entry in result:
+            num_entries += 1
             self.assertEqual(entry["protein"]["name"], "CCR5")
             association = entry["association"]
 
             if entry["other"] is not None:
-                self.assertIn(entry["other"]["name"], ["CCL5", "CCR5", "IL10RA"])
                 
-                if entry["other"]["preferred_name"] == "CCL5":
+                if entry["other"]["name"] == "CCL5":
+                    has_ccl5_other = True
                     self.assertEqual(association["coexpression"], 92)
                     self.assertEqual(association["database"], 900)
                     self.assertEqual(association["textmining"], 906)
                     self.assertEqual(association["combined"], 993)
 
-                    if entry["action"] is not None:
-                        self.assertIn(entry["action"]["mode"], [
-                            "ptmod",
-                            "reaction",
-                            "binding",
-                            "catalysis",
-                            "activation"
-                        ])
-                        action = entry["action"]
+                    # if entry["action"] is not None:
+                    #     self.assertIn(entry["action"]["mode"], [
+                    #         "ptmod",
+                    #         "reaction",
+                    #         "binding",
+                    #         "catalysis",
+                    #         "activation"
+                    #     ])
+                    #     action = entry["action"]
 
-                        if action["mode"] == "binding":
-                            self.assertEqual(action["score"], 849)
-                        elif action["mode"] == "ptmod":
-                            self.assertEqual(action["score"], 171)
-                        elif action["mode"] == "catalysis":
-                            self.assertEqual(action["score"], 278)
-                        elif action["mode"] == "reaction":
-                            self.assertEqual(action["score"], 278)
-                        elif action["mode"] == "activation":
-                            self.assertEqual(action["score"], 849)
+                    #     if action["mode"] == "binding":
+                    #         self.assertEqual(action["score"], 849)
+                    #     elif action["mode"] == "ptmod":
+                    #         self.assertEqual(action["score"], 171)
+                    #     elif action["mode"] == "catalysis":
+                    #         self.assertEqual(action["score"], 278)
+                    #     elif action["mode"] == "reaction":
+                    #         self.assertEqual(action["score"], 278)
+                    #     elif action["mode"] == "activation":
+                    #         self.assertEqual(action["score"], 849)
 
-                    elif entry["other"]["name"] == "IL10RA":
-                        self.assertEqual(association["coexpression"], 246) 
-                        self.assertEqual(association["textmining"], 318)
-                        self.assertEqual(association["combined"], 469)
+                elif entry["other"]["name"] == "IL10RA":
+                    has_il10ra_other = True
+                    self.assertEqual(association["coexpression"], 246) 
+                    self.assertEqual(association["textmining"], 318)
+                    self.assertEqual(association["combined"], 469)
 
             if entry["pathway"] is not None:
+                has_pathway = True
                 self.assertIn(entry["pathway"]["name"], [
                     "Toll-like receptor signaling pathway",
                     "NOD-like receptor signaling pathway",
@@ -75,62 +82,10 @@ class TestCypherQueries(unittest.TestCase):
                     "Viral carcinogenesis"
                 ])
 
-                self.assertIsNone(entry["drug"])
-                self.assertIsNone(entry["disease"])
-
-                if entry["compound"] is not None:
-                    self.assertIn(entry["compound"]["name"], [
-                        "L-Homocysteine",
-                        "Potassium cation",
-                        "Nitric oxide",
-                        "Defensin alpha-1",
-                        "Defensin alpha-2",
-                        "Defensin alpha-3",
-                        "Defensin alpha-4",
-                        "Defensin alpha-5",
-                        "Defensin beta-1",
-                        "Defensin beta-2",
-                        "Defensin beta-3",
-                        "Defensin beta-4",
-                        "Hydrogen peroxide",
-                        "Corticosterone",
-                        "L-Arginine",
-                        "Bradykinin",
-                        "H+",
-                        "Vitamin D3",
-                        "Parathyroid hormone",
-                        "Lipopolysaccharide",
-                        "1-Phosphatidyl-D-myo-inositol",
-                        "Calcidiol",
-                        "Calcitriol",
-                        "Sphingosine 1-phosphate",
-                        "Trehalose-6,6'-dibehenate",
-                        "Trehalose dimycolate",
-                        "Lipomannan",
-                        "Triacylated phosphatidyl-myo-inositol pentamannoside",
-                        "Triacylated phosphatidyl-myo-inositol hexamannoside",
-                        "Triacylated phosphatidyl-myo-inositol dimannoside",
-                        "Tetraacylated phosphatidyl-myo-inositol dimannoside",
-                        "Tetraacylated phosphatidyl-myo-inositol hexamannoside",
-                        "Tetraacylated phosphatidyl-myo-inositol pentamannoside",
-                        "Lipoarabinomannan",
-                        "Mannose-capped lipoarabinomannan",
-                        "Calcium cation",
-                        "Diacylglycerol",
-                        "3',5'-Cyclic AMP",
-                        "D-myo-Inositol 1,4,5-trisphosphate",
-                        "Phosphatidylinositol-3,4,5-trisphosphate",
-                        "GDP",
-                        "GTP",
-                        "1-Phosphatidyl-1D-myo-inositol 3-phosphate",
-                        "1-Phosphatidyl-D-myo-inositol 4,5-bisphosphate",
-                        "1-Phosphatidyl-1D-myo-inositol 3,5-bisphosphate",
-                        "Lipoxin A4",
-                        "Prostaglandin E2",
-                        "Cyclic GMP-AMP",
-                        "Phosphatidylethanolamine",
-                        "ATP"
-                    ])
+        self.assertGreater(num_entries, 0)
+        self.assertTrue(has_ccl5_other)
+        self.assertTrue(has_il10ra_other)
+        self.assertTrue(has_pathway)
 
 if __name__ == "__main__":
     print("Testing Cypher queries...")
