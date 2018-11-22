f6df8d31e612f07bab81ec277b0b06ad578116b6
Borna Bešić <besic.borna@gmail.com>
2018-11-22 11:32:57 +0100

    Refactor utils.py

--- utils.py
+++ utils.py
@@ -1,21 +1,45 @@
+"""
+Collection of useful functions.
+"""
 
-def rstrip_line_generator(iterable, skip_empty = False):
+def rstrip_line_generator(iterable, skip_empty=False):
+    """
+    Generator which iterates over lines provided by the
+    file handle but strips '\n' from the right of each
+    line.
+    """
+
     for line in iterable:
         new_line = line.rstrip("\n")
-        if skip_empty and new_line != "":	
+        if skip_empty and new_line != "":
             yield new_line
 
-def lines(buffer, encoding = "utf-8"):
+def lines(buffer, encoding="utf-8"):
+    """
+    Generator which iterates over text lines in the
+    buffer. The buffer can either be a string or a file
+    handle.
+    """
+
     if "\n" in buffer: # If it is a string, split by \n
         iterable = buffer.split("\n")
-        for rstripped_line in rstrip_line_generator(iterable, skip_empty = True):
-                yield rstripped_line
+        for rstripped_line in rstrip_line_generator(iterable, skip_empty=True):
+            yield rstripped_line
     else: # else assume it is a file handle
-        with open(buffer, "rt", encoding = encoding) as iterable:
-            for rstripped_line in rstrip_line_generator(iterable, skip_empty = True):
+        with open(buffer, "rt", encoding=encoding) as iterable:
+            for rstripped_line in rstrip_line_generator(iterable, skip_empty=True):
                 yield rstripped_line
 
-def read_table(file_path, types, delimiter = ",", header = False):
+def read_table(file_path, types, delimiter=",", header=False):
+    """
+    Read a text file as a table (e.g. TSV if delimiter set to '\t')
+    and automatically converts the cells to the specified types.
+
+    'types' should be an iterable containing data type constructors
+    and its length should be the same as the number of expected columns.
+    """
+
+    types = tuple(types)
     expected_cols = len(types)
     for line in lines(file_path):
         if header:
@@ -31,12 +55,20 @@ def read_table(file_path, types, delimiter = ",", header = False):
             pass
 
 def pair_generator(elements):
+    """
+    Generate pairs of elements from a list of elements.
+    """
+
     n = len(elements)
     for i in range(n):
         for j in range(i + 1, n):
             yield elements[i], elements[j]
 
 def batches(generator, batch_size):
+    """
+    Generate batches of data from the given generator.
+    """
+
     batch = []
     for item in generator:
         batch.append(item)
@@ -46,6 +78,11 @@ def batches(generator, batch_size):
     yield batch
 
 def concat(list_of_lists):
+    """
+    Concatenate lists from the list ('list_of_lists')
+    into a single list.
+    """
+
     for list in list_of_lists:
         for item in list:
             yield item
