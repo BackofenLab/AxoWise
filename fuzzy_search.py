57da178821ab13ad4438fccfbb131bd82d6235e4
Borna Bešić <besic.borna@gmail.com>
2018-11-30 10:43:14 +0100

    Add a code skeleton of a fuzzy search module

---
+++ fuzzy_search.py
@@ -0,0 +1,31 @@
+"""
+Collection of functions that handle user queries and
+suggest top matches based on fuzzy search techniques.
+"""
+
+# ========================= Species =========================
+
+def search_species(query):
+    """
+    Retrieve top-matching species for a given
+    species name or NCBI ID.
+    """
+    pass
+
+# ========================= Protein =========================
+
+def search_protein(query):
+    """
+    Retrieve top-matching proteins for a given
+    protein name or Ensembl ID.
+    """
+    pass
+
+# ========================= Pathway =========================
+
+def search_pathway(query):
+    """
+    Retrieve top-matching pathways for a given
+    pathway name or KEGG ID.
+    """
+    pass
