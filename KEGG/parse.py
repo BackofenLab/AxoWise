f4ddec5e977c84b77bf2c5bf21cee00505f558e3
Borna Bešić <besic.borna@gmail.com>
2018-12-06 13:58:02 +0100

    Remove KGML parsing

--- KEGG/parse.py
+++ KEGG/parse.py
@@ -1,19 +1,6 @@
 
 import re
 import io
-from Bio.KEGG.KGML import KGML_parser
-
-def parse_KGML(pathway):
-    gene_ids = set()
-    pathway_stream = io.StringIO(pathway)
-    pathway_object = KGML_parser.read(pathway_stream)
-
-    for entry in pathway_object.genes:
-        gene_ids.update(entry.name.split(" "))
-
-    gene_ids = list(gene_ids)
-
-    return pathway_object.title, gene_ids
 
 def parse_flat_file(pathway):
     name = None
