fa0da51bc86c0053571c56631dcbd0e61b025c8f
Borna Bešić <besic.borna@gmail.com>
2019-05-23 11:15:47 +0200

    Fix test_get_protein_subgraph()

--- tests/test_cypher_queries.py
+++ tests/test_cypher_queries.py
@@ -11,83 +11,62 @@ class TestCypherQueries(unittest.TestCase):
 
         protein_id = fuzzy_search.search_protein("ccr5", context.SPECIES_ID)[0].id
 
-        result = Cypher.get_protein_subgraph(neo4j_graph, protein_id)
+        result = list(Cypher.get_protein_subgraph(neo4j_graph, protein_id))
 
-        num_entries = 0
-        has_pathway = False
         has_ccl5_other = False
         has_il10ra_other = False
-        for entry in result:
-            num_entries += 1
-            self.assertEqual(entry["protein"]["name"], "CCR5")
-            association = entry["association"]
 
-            if entry["other"] is not None:
-                
-                if entry["other"]["name"] == "CCL5":
-                    has_ccl5_other = True
-                    self.assertEqual(association["coexpression"], 92)
-                    self.assertEqual(association["database"], 900)
-                    self.assertEqual(association["textmining"], 906)
-                    self.assertEqual(association["combined"], 993)
+        self.assertEqual(len(result), 1)
+        data = result[0]
 
-                    # if entry["action"] is not None:
-                    #     self.assertIn(entry["action"]["mode"], [
-                    #         "ptmod",
-                    #         "reaction",
-                    #         "binding",
-                    #         "catalysis",
-                    #         "activation"
-                    #     ])
-                    #     action = entry["action"]
+        protein = data["protein"]
+        self.assertEqual(protein["name"], "CCR5")
 
-                    #     if action["mode"] == "binding":
-                    #         self.assertEqual(action["score"], 849)
-                    #     elif action["mode"] == "ptmod":
-                    #         self.assertEqual(action["score"], 171)
-                    #     elif action["mode"] == "catalysis":
-                    #         self.assertEqual(action["score"], 278)
-                    #     elif action["mode"] == "reaction":
-                    #         self.assertEqual(action["score"], 278)
-                    #     elif action["mode"] == "activation":
-                    #         self.assertEqual(action["score"], 849)
+        associations = data["associations"]
+        for association in associations:
+            combined_score = association["combined_score"]
+            other = association["other"]
 
-                elif entry["other"]["name"] == "IL10RA":
-                    has_il10ra_other = True
-                    self.assertEqual(association["coexpression"], 246) 
-                    self.assertEqual(association["textmining"], 318)
-                    self.assertEqual(association["combined"], 469)
+            if other["name"] == "CCL5":
+                has_ccl5_other = True
+                # self.assertEqual(association["coexpression"], 92)
+                # self.assertEqual(association["database"], 900)
+                # self.assertEqual(association["textmining"], 906)
+                self.assertEqual(combined_score, 993)
 
-            if entry["pathways"] is not None:
-                has_pathway = True
-                for pathway in entry["pathways"]:
-                    self.assertIn(pathway["name"], [
-                        "Toll-like receptor signaling pathway",
-                        "NOD-like receptor signaling pathway",
-                        "Cytosolic DNA-sensing pathway",
-                        "TNF signaling pathway",
-                        "Prion diseases",
-                        "Chagas disease (American trypanosomiasis)",
-                        "Influenza A",
-                        "Herpes simplex infection",
-                        "Rheumatoid arthritis",
-                        "JAK-STAT signaling pathway",
-                        "Tuberculosis",
-                        "Epstein-Barr virus infection",
-                        "Cytokine-cytokine receptor interaction",
-                        "Chemokine signaling pathway",
-                        "Endocytosis",
-                        "Toxoplasmosis",
-                        "Human cytomegalovirus infection",
-                        "Kaposi sarcoma-associated herpesvirus infection",
-                        "Human immunodeficiency virus 1 infection",
-                        "Viral carcinogenesis"
-                    ])
+            elif other["name"] == "IL10RA":
+                has_il10ra_other = True
+                #self.assertEqual(association["coexpression"], 246)
+                #self.assertEqual(association["textmining"], 318)
+                self.assertEqual(combined_score, 469)
 
-        self.assertGreater(num_entries, 0)
+        pathways = data["pathways"]
+        for pathway in pathways:
+            self.assertIn(pathway["name"], [
+                "Toll-like receptor signaling pathway",
+                "NOD-like receptor signaling pathway",
+                "Cytosolic DNA-sensing pathway",
+                "TNF signaling pathway",
+                "Prion diseases",
+                "Chagas disease (American trypanosomiasis)",
+                "Influenza A",
+                "Herpes simplex infection",
+                "Rheumatoid arthritis",
+                "JAK-STAT signaling pathway",
+                "Tuberculosis",
+                "Epstein-Barr virus infection",
+                "Cytokine-cytokine receptor interaction",
+                "Chemokine signaling pathway",
+                "Endocytosis",
+                "Toxoplasmosis",
+                "Human cytomegalovirus infection",
+                "Kaposi sarcoma-associated herpesvirus infection",
+                "Human immunodeficiency virus 1 infection",
+                "Viral carcinogenesis"
+            ])
+
         self.assertTrue(has_ccl5_other)
         self.assertTrue(has_il10ra_other)
-        self.assertTrue(has_pathway)
 
     def test_get_pathway_subgraph(self):
         neo4j_graph = database.connect_neo4j(context.CREDENTIALS_FILE_PATH)
