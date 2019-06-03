e31c56833504b815702fd807b917f06fbafa4f3f
Borna Bešić <besic.borna@gmail.com>
2019-06-03 11:20:18 +0200

    Use @throttle for url.get()

--- url.py
+++ url.py
@@ -6,10 +6,12 @@ import time
 import urllib.error
 from urllib.request import urlopen
 
+from utils import throttle
 
-def get(url, timeout = 10, wait = 1):
-    if isinstance(wait, int) and wait > 0:
-        time.sleep(wait)
+_MIN_WAIT = 1 # second(s)
+
+@throttle(_MIN_WAIT)
+def get(url, timeout = 10):
     try:
         return urlopen(url, timeout = timeout).read().decode(encoding = "utf-8", errors = "ignore")
     except Exception as e:
