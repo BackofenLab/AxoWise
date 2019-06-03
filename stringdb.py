4d6183942544923b1f3dfa705a02d26a0b473241
Borna Bešić <besic.borna@gmail.com>
2019-06-03 10:59:16 +0200

    Update stringdb.py

--- stringdb.py
+++ stringdb.py
@@ -15,13 +15,22 @@ _SEPARATOR = "%0d"
 # =================================== Functional enrichment ===================================
 # https://string-db.org/cgi/help.pl?subpage=api%23getting-functional-enrichment
 
-functional_enrichment_template = Template("https://string-db.org/api/${format}/enrichment?identifiers=${identifiers}&species=${species}")
+functional_enrichment_template = Template("https://string-db.org/api/${format}/enrichment?identifiers=${identifiers}&species=${species}&caller_identity=${identity}")
 
 def functional_enrichment(identifiers, species_id):
+    """
+    Retrieves the functional enrichment for any set of input proteins.
+
+    Arguments:
+    - identifiers - protein identifiers (preferably mapped via STRING'S API before)
+    - species_id - NCBI taxon identifier
+    """
+
     endpoint = functional_enrichment_template.substitute(
         format = "tsv",
         identifiers = _SEPARATOR.join(identifiers),
-        species = species_id
+        species = species_id,
+        identity = _CALLER_IDENTITY
     )
 
     enrichment_file = get(endpoint)
@@ -29,5 +38,6 @@ def functional_enrichment(identifiers, species_id):
         return None
 
     df = pd.read_csv(StringIO(enrichment_file), sep="\t")
+    del df["ncbiTaxonId"], df["number_of_genes"], df["preferredNames"]
 
     return df
