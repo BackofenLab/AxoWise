e8be1ba087d28227b283363f193816be5483aa6f
Borna Bešić <besic.borna@gmail.com>
2019-06-05 11:53:42 +0200

    Use OpenCSV to read the graph definition from the standard input

--- gephi-backend/src/main/java/Main.java
+++ gephi-backend/src/main/java/Main.java
@@ -1,6 +1,6 @@
-import org.gephi.graph.api.DirectedGraph;
-import org.gephi.graph.api.GraphController;
-import org.gephi.graph.api.GraphModel;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderHeaderAware;
+import org.gephi.graph.api.*;
 import org.gephi.io.exporter.api.ExportController;
 import org.gephi.io.exporter.plugin.ExporterCSV;
 import org.gephi.io.generator.plugin.RandomGraph;
@@ -14,7 +14,8 @@ import org.gephi.project.api.Workspace;
 import org.openide.util.Lookup;
 
 import java.io.*;
-import java.util.concurrent.TimeUnit;
+import java.util.Map;
+
 
 public class Main {
 
@@ -24,46 +25,94 @@ public class Main {
         pc.newProject();
         Workspace workspace = pc.getCurrentWorkspace();
 
-        // TODO Read graph from the standard input into a container
+        // Read graph from the standard input into a container
         Container container = Lookup.getDefault().lookup(Container.Factory.class).newContainer();
-        RandomGraph randomGraph = new RandomGraph();
-        randomGraph.setNumberOfNodes(500);
-        randomGraph.setWiringProbability(0.005);
-        randomGraph.generate(container.getLoader());
-
-        // Append container to graph structure
-        ImportController importController = Lookup.getDefault().lookup(ImportController.class);
-        importController.process(container, new DefaultProcessor(), workspace);
-
-        // See if graph is well imported
         GraphModel graphModel = Lookup.getDefault().lookup(GraphController.class).getGraphModel();
-        DirectedGraph graph = graphModel.getDirectedGraph();
-        System.out.println("Nodes: " + graph.getNodeCount());
-        System.out.println("Edges: " + graph.getEdgeCount());
+        GraphFactory graphFactory = graphModel.factory();
+        UndirectedGraph undirectedGraph = graphModel.getUndirectedGraph();
 
-        // Layout for 1 minute
-        AutoLayout autoLayout = new AutoLayout(1, TimeUnit.MINUTES);
-        autoLayout.setGraphModel(graphModel);
+        InputStreamReader stdinReader = new InputStreamReader(System.in);
+        CSVReaderHeaderAware csvReader;
+        Map<String, String> nextRecord;
 
-        // Force Atlas layout
-        ForceAtlasLayout secondLayout = new ForceAtlasLayout(null);
-        AutoLayout.DynamicProperty adjustBySizeProperty = AutoLayout.createDynamicProperty("forceAtlas.adjustSizes.name", Boolean.TRUE, 0.1f);//True after 10% of layout time
-        AutoLayout.DynamicProperty repulsionProperty = AutoLayout.createDynamicProperty("forceAtlas.repulsionStrength.name", 500., 0f);//500 for the complete period
-        autoLayout.addLayout(secondLayout, 1.f, new AutoLayout.DynamicProperty[]{adjustBySizeProperty, repulsionProperty});
+        try  {
+            csvReader = new CSVReaderHeaderAware(stdinReader);
+            // Nodes
+            while ((nextRecord = csvReader.readMap()) != null) {
+                String id = nextRecord.get("id");
+                String external_id = nextRecord.get("external_id");
+                String name = nextRecord.get("name");
+                String description = nextRecord.get("description");
 
-        autoLayout.execute();
+                Node n = graphFactory.newNode(id);
+                n.setLabel(name);
+                n.setAttribute("external_id", external_id);
+                n.setAttribute("description", description);
 
-        // Print to the standard output as CSV
-        ExportController ec = Lookup.getDefault().lookup(ExportController.class);
+                undirectedGraph.addNode(n);
 
-        ExporterCSV exporterCSV = (ExporterCSV) ec.getExporter("csv");
-        exporterCSV.setExportVisible(true);
-        exporterCSV.setWorkspace(workspace);
+                System.out.println(nextRecord);
+            }
 
-        StringWriter writer = new StringWriter();
+            System.err.println("Nodes:" + undirectedGraph.getNodeCount());
+        }
+        catch (IOException ex) {}
 
-        ec.exportWriter(writer, exporterCSV);
-        System.out.print(writer.toString());
+
+        try {
+            csvReader = new CSVReaderHeaderAware(stdinReader);
+            // Edges
+            while ((nextRecord = csvReader.readMap()) != null) {
+                String source = nextRecord.get("source");
+                String target = nextRecord.get("target");
+                String score = nextRecord.get("score");
+
+                Node n1 = undirectedGraph.getNode(source);
+                Node n2 = undirectedGraph.getNode(target);
+                Edge e = graphFactory.newEdge(n1, n2);
+                e.setAttribute("score", score);
+
+                undirectedGraph.addEdge(e);
+
+                System.out.println(nextRecord);
+            }
+
+            System.err.println("Edges:" + undirectedGraph.getEdgeCount());
+        }
+        catch (IOException ex) {}
+
+//        // Append container to graph structure
+//        ImportController importController = Lookup.getDefault().lookup(ImportController.class);
+//        importController.process(container, new DefaultProcessor(), workspace);
+//
+//        // See if graph is well imported
+//        DirectedGraph graph = graphModel.getDirectedGraph();
+//        System.out.println("Nodes: " + graph.getNodeCount());
+//        System.out.println("Edges: " + graph.getEdgeCount());
+//
+//        // Layout for 1 minute
+//        AutoLayout autoLayout = new AutoLayout(1, TimeUnit.MINUTES);
+//        autoLayout.setGraphModel(graphModel);
+//
+//        // Force Atlas layout
+//        ForceAtlasLayout secondLayout = new ForceAtlasLayout(null);
+//        AutoLayout.DynamicProperty adjustBySizeProperty = AutoLayout.createDynamicProperty("forceAtlas.adjustSizes.name", Boolean.TRUE, 0.1f);//True after 10% of layout time
+//        AutoLayout.DynamicProperty repulsionProperty = AutoLayout.createDynamicProperty("forceAtlas.repulsionStrength.name", 500., 0f);//500 for the complete period
+//        autoLayout.addLayout(secondLayout, 1.f, new AutoLayout.DynamicProperty[]{adjustBySizeProperty, repulsionProperty});
+//
+//        autoLayout.execute();
+//
+//        // Print to the standard output as CSV
+//        ExportController ec = Lookup.getDefault().lookup(ExportController.class);
+//
+//        ExporterCSV exporterCSV = (ExporterCSV) ec.getExporter("csv");
+//        exporterCSV.setExportVisible(true);
+//        exporterCSV.setWorkspace(workspace);
+//
+//        StringWriter writer = new StringWriter();
+//
+//        ec.exportWriter(writer, exporterCSV);
+//        System.out.print(writer.toString());
     }
 
 
