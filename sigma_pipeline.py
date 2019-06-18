004dc78c6a21223a0c94dd5d4547bf0eeaefda3c
Borna Bešić <besic.borna@gmail.com>
2019-06-18 10:38:56 +0200

    Update the data pipeline skeleton

--- sigma_pipeline.py
+++ sigma_pipeline.py
@@ -1,75 +1,83 @@
 
-import jar
-import fuzzy_search
-import cypher_queries as Cypher
-import database
+import io
+import json
+import time
+from contextlib import contextmanager
+
 import pandas as pd
 
-import time
-start = time.time()
+import cypher_queries as Cypher
+import database
+import fuzzy_search
+import jar
+
+
+@contextmanager
+def measure_time(what):
+    start = time.time()
+    yield
+    end = time.time()
+    print(f"[{what}] {end - start:.2f} s")
 
 with open("../genes.clean.txt", "r") as f:
     genes = list(map(lambda line: line.rstrip("\n"), f))
 
-proteins = fuzzy_search.search_protein_list(genes, species_id=9606)
-end = time.time()
-print(end - start, "s for fuzzy search")
+with measure_time("Fuzzy search"):
+    proteins = fuzzy_search.search_protein_list(genes, species_id=9606)
 
 protein_ids = list(map(lambda p: p.id, proteins))
-print(len(protein_ids))
 
 query = """
-    MATCH (source:Protein)-[association:ASSOCIATION]-(target:Protein)
-    WHERE association.combined >= {threshold} AND source.id IN {protein_ids} AND source.id IN {protein_ids} AND source.id < target.id
+    MATCH (source:Protein)-[association:ASSOCIATION]->(target:Protein)
+    WHERE source.id IN {protein_ids} AND target.id IN {protein_ids} AND association.combined >= {threshold}
     RETURN source, target, association.combined AS score
 """
 
 param_dict = dict(
     protein_ids=protein_ids,
-    threshold=400
+    threshold=600
 )
 
 
-start = time.time()
-data =  database.neo4j_graph.data(query, param_dict)
-end = time.time()
-print(end - start, "s for Cypher query")
+with measure_time("Cypher query"):
+    data =  database.neo4j_graph.data(query, param_dict)
 
-start = time.time()
-proteins = set()
-source, target, score = list(), list(), list()
-for row in data:
-    source.append(row["source"]["id"])
-    target.append(row["target"]["id"])
-    score.append(row["score"])
-    proteins.add(row["source"])
-    proteins.add(row["target"])
+with measure_time("pandas DataFrames"):
+    proteins = set()
+    source, target, score = list(), list(), list()
+    for row in data:
+        source.append(row["source"]["id"])
+        target.append(row["target"]["id"])
+        score.append(row["score"])
+        proteins.add(row["source"])
+        proteins.add(row["target"])
 
-proteins = list(map(dict, proteins))
+    proteins = list(map(dict, proteins))
 
-nodes = pd.DataFrame(proteins)
-edges = pd.DataFrame({
-    "source": source,
-    "target": target,
-    "score": score
-})
-end = time.time()
-print(end - start, "s for creating pandas DataFrames")
+    nodes = pd.DataFrame(proteins)
+    edges = pd.DataFrame({
+        "source": source,
+        "target": target,
+        "score": score
+    })
 
-start = time.time()
-import io
-nodes_csv = io.StringIO()
-edges_csv = io.StringIO()
-nodes.to_csv(nodes_csv, index=False)
-edges.to_csv(edges_csv, index=False)
-nodes.to_csv("nodes.csv", index=False)
-edges.to_csv("edges.csv", index=False)
-end = time.time()
-print(end - start, "s for writing CSV to string")
+with measure_time("CSV"):
+    nodes_csv = io.StringIO()
+    edges_csv = io.StringIO()
 
-stdin = f"{nodes_csv.getvalue()}\n{edges_csv.getvalue()}"
+    # NODES
+    nodes = nodes.drop_duplicates(subset="id")
+    # JAR accepts only id
+    nodes["id"].to_csv(nodes_csv, index=False, header=True)
 
-start = time.time()
-stdout = jar.pipe_call("gephi-backend/out/artifacts/gephi_backend_jar/gephi.backend.jar", stdin)
-end = time.time()
-print(end - start, "s for calling the JAR")
+    # EDGES
+    edges = edges.drop_duplicates(subset=["source", "target"])
+    # JAR accepts source, target, score
+    edges.to_csv(edges_csv, index=False, header=True)
+
+    stdin = f"{nodes_csv.getvalue()}\n{edges_csv.getvalue()}"
+
+with measure_time("JAR"):
+    stdout = jar.pipe_call("gephi-backend/out/artifacts/gephi_backend_jar/gephi.backend.jar", stdin)
+
+sigmajs_data = json.loads(stdout)
