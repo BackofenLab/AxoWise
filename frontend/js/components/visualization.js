99156347c751e7d3a237700de109500bbfbd2f72
Borna Bešić <besic.borna@gmail.com>
2019-06-25 09:57:58 +0200

    Recover the colors after the term view

--- frontend/js/components/visualization.js
+++ frontend/js/components/visualization.js
@@ -1,3 +1,7 @@
+
+// Sigma.js
+
+// THIS INSTANCE OF SIGMA IS GLOBAL
 var sigma_instance = null;
 
 sigma.classes.graph.addMethod('getNodeFromIndex', function(id) {
@@ -14,6 +18,7 @@ sigma.classes.graph.addMethod('ensemblIdToNode', function(ensembl_id) {
     return null;
 });
 
+// Component
 Vue.component("visualization", {
     props: ["gephi_json", "active_node", "active_term"],
     data: function() {
@@ -104,84 +109,6 @@ Vue.component("visualization", {
         }
     },
     methods: {
-        // Rectangular select callbacks
-        mousedown: function(e) {
-            var com = this;
-            if (e.button == 2) {
-                var selectedNodes = e.ctrlKey ? NETWORK.getSelectedNodes() : null;
-                com.backup_surface();
-                var that = this;
-                var rectangle = com.rectangular_select.rectangle;
-                rectangle.startX = e.pageX - $(com.container)[0].offsetLeft;
-                rectangle.startY = e.pageY - $(com.container)[0].offsetTop;
-                com.rectangular_select.active = true;
-                $(com.container)[0].style.cursor = "crosshair";
-            }
-        },
-        mousemove: function(e) {
-            var com = this;
-            if (com.rectangular_select.active) {
-                var context = com.rectangular_select.context;
-                var rectangle = com.rectangular_select.rectangle;
-                com.restore_surface();
-                rectangle.w = (e.pageX - $(com.container)[0].offsetLeft) - rectangle.startX;
-                rectangle.h = (e.pageY - $(com.container)[0].offsetTop) - rectangle.startY ;
-                context.setLineDash([5]);
-                context.strokeStyle = "rgb(82,182,229)";
-                context.strokeRect(rectangle.startX, rectangle.startY, rectangle.w, rectangle.h);
-                context.setLineDash([]);
-                context.fillStyle = "rgba(82,182,229,0.2)";
-                context.fillRect(rectangle.startX, rectangle.startY, rectangle.w, rectangle.h);
-            }
-        },
-        mouseup: function(e) {
-            var com = this;
-            if (e.button == 2) {
-                com.restore_surface();
-                com.rectangular_select.active = false;
-
-                $(com.container)[0].style.cursor = "default";
-                com.select_nodes_rectangular();
-            }
-        },
-        backup_surface: function() {
-            var com = this;
-            var canvas = com.rectangular_select.canvas;
-            var context = com.rectangular_select.context;
-            com.rectangular_select.surface_backup = context.getImageData(0, 0, canvas.width, canvas.height);
-        },
-        restore_surface: function() {
-            var com = this;
-            var context = com.rectangular_select.context;
-            var surface = com.rectangular_select.surface_backup;
-            context.putImageData(surface, 0, 0);
-        },
-        select_nodes_rectangular: function() {
-            var com = this;
-            if(!com.current_data_node.data) return;
-            var rectangle = com.rectangular_select.rectangle;
-
-            var selected_nodes = [];
-            var x_range = com.get_select_range(rectangle.startX, rectangle.w);
-            var y_range = com.get_select_range(rectangle.startY, rectangle.h);
-
-            var nodes = com.current_data_node.data.nodes_protein.get();
-            for (var i = 0; i < nodes.length; i++) {
-                var node = nodes[i];
-                var node_position = NETWORK.getPositions([node.id]);
-                if (!node_position[node.id]) continue;
-
-                var node_XY = NETWORK.canvasToDOM({x: node_position[node.id].x, y: node_position[node.id].y});
-                if (x_range.start <= node_XY.x && node_XY.x <= x_range.end && y_range.start <= node_XY.y && node_XY.y <= y_range.end) {
-                    selected_nodes.push(node.id);
-                }
-            }
-            NETWORK.selectNodes(selected_nodes);
-        },
-        get_select_range: function(start, length) {
-            return length > 0 ? {start: start, end: start + length} : {start: start + length, end: start};
-        },
-
         normal_node: function() {
             var com = this;
 
@@ -224,12 +151,6 @@ Vue.component("visualization", {
         sigma_instance.bind("clickNode", function (node) {
             com.$emit("active-node-changed", node.data.node.id);
         });
-        // com.search = new Search(com.form.find("#search"));
-
-        // rectangular select
-        // container.oncontextmenu = function() { return false; };
-        // com.rectangular_select.canvas = NETWORK.canvas.frame.canvas;
-        // com.rectangular_select.context = com.rectangular_select.canvas.getContext("2d");
     },
     template: `
     <div class="sigma-parent">
