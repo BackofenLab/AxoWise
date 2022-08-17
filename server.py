f962e7f0581e6b45eaffb9ba9665c13eca2880ae
Nico Greiner <nico.greiner9@gmail.com>
2022-08-17 10:40:42 +0200

    -Implemented direct Cypher Shell access for database runtime improvement
    - Implemented timer to keep track of the runtime of seperate parts
    - Implement subprocess.run() to execute the cypher-shell command
    - We're creating csv file with cypher and open/extract it to python

--- server.py
+++ server.py
@@ -1,7 +1,13 @@
+import ast
+from asyncio import subprocess
+import subprocess
 import json
 import os.path
 import io
 from collections import defaultdict
+import csv
+from sys import stderr
+from threading import Timer
 
 # import networkx as nx
 from flask import Flask, Response, request, send_from_directory
@@ -17,6 +23,7 @@ from layout import shell_layout
 
 import graph_utilities
 
+import time
 app = Flask(__name__)
 
 # ====================== Index page ======================
@@ -41,6 +48,10 @@ def files(path):
 # TODO Refactor this
 @app.route("/api/subgraph/proteins", methods=["POST"])
 def proteins_subgraph_api():
+
+    #Begin a timer to time
+    t_begin = time.time()
+
     # Queried proteins
     query_proteins = request.form.get("proteins").split(";")
     query_proteins = list(filter(None, query_proteins))
@@ -55,35 +66,89 @@ def proteins_subgraph_api():
     proteins = fuzzy_search.search_protein_list(query_proteins, species_id=species_id)
     protein_ids = list(map(lambda p: p.id, proteins))
 
-    # Query the database
-    query = """
-        MATCH (source:Protein)-[association:ASSOCIATION]->(target:Protein)
-        WHERE source.id IN {protein_ids} AND target.id IN {protein_ids} AND association.combined >= {threshold}
-        RETURN source, target, association.combined AS score
-    """
+    # Create a query to find all associations between protein_ids and create a file with all properties
+    def create_query_assoc():
+        
+        query = """
+                WITH "MATCH (source:Protein)-[association:ASSOCIATION]->(target:Protein)
+                WHERE source.id IN
+                """ + repr(protein_ids) + ' AND target.id IN ' + repr(protein_ids) + ' AND association.combined >= ' + repr(threshold) + """
+                RETURN source, target, association.combined AS score" AS query
+                CALL apoc.export.csv.query(query, "/tmp/neo4j_output.csv", {})
+                YIELD file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data
+                RETURN file, source, format, nodes, relationships, properties, time, rows, batchSize, batches, done, data;
+                """
+        return query
+        
+    query = create_query_assoc()
 
-    param_dict = dict(
-        protein_ids=protein_ids,
-        threshold=threshold
+    #Timer to evaluate runtime to setup
+    t_setup = time.time()
+    print("Time Spent (Setup):", t_setup-t_begin)
+
+    #---------Start py2neo
+
+    # # Query the database
+    # query = """
+    #     MATCH (source:Protein)-[association:ASSOCIATION]->(target:Protein)
+    #     WHERE source.id IN {protein_ids} AND target.id IN {protein_ids} AND association.combined >= {threshold}
+    #     RETURN source, target, association.combined AS score
+    # """
+
+    # param_dict = dict(
+    #     protein_ids=protein_ids,
+    #     threshold=threshold
+    # )
+
+    # data = database.neo4j_graph.run(query, param_dict).data()
+
+    # #no data from database, return from here
+    # # TO-DO Front end response to be handled
+    # if not data:
+    #     return Response(json.dumps([]), mimetype="application/json")
+    
+    # #pandas DataFrames for nodes and edges
+    # proteins = set()
+    # source, target, score = list(), list(), list()
+    # for row in data:
+    #     source.append(row["source"]["id"])
+    #     target.append(row["target"]["id"])
+    #     score.append(row["score"])
+    #     proteins.add(row["source"])
+    #     proteins.add(row["target"])
+
+    # proteins = list(map(dict, proteins))
+
+    #---------End py2neo
+
+    #Run the cypher query in cypher shell via terminal
+    data = subprocess.run(
+        ["cypher-shell",
+         "-a", "bolt://localhost:7687",
+         "-u", "neo4j",
+         "-p", "pgdb",
+         query],
+        capture_output=True,
+        encoding="utf-8"
     )
 
-    data = database.neo4j_graph.run(query, param_dict).data()
-    #no data from database, return from here
-    # TO-DO Front end response to be handled
-    if not data:
-        return Response(json.dumps([]), mimetype="application/json")
+    #Timer for Neo4j query
+    t_neo4j = time.time()
+    print("Time Spent (Neo4j):", t_neo4j-t_setup)
 
-    # pandas DataFrames for nodes and edges
-    proteins = set()
+    #pandas DataFrames for nodes and edges
+    proteins = list()
     source, target, score = list(), list(), list()
-    for row in data:
-        source.append(row["source"]["id"])
-        target.append(row["target"]["id"])
-        score.append(row["score"])
-        proteins.add(row["source"])
-        proteins.add(row["target"])
-
-    proteins = list(map(dict, proteins))
+    with open('/tmp/neo4j_output.csv', newline='') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            source_row, target_row = ast.literal_eval(row['source']), ast.literal_eval(row['target'])
+            source_row_prop, target_row_prop = source_row.get('properties'), target_row.get('properties')
+            source.append(source_row_prop.get('id'))
+            target.append(target_row_prop.get('id'))
+            score.append(int(row['score']))
+            proteins.append(source_row_prop)
+            proteins.append(target_row_prop)
 
     nodes = pd.DataFrame(proteins)
     nodes = nodes.drop_duplicates(subset="id") # TODO `nodes` can be empty
@@ -94,6 +159,16 @@ def proteins_subgraph_api():
         "score": score
     })
     edges = edges.drop_duplicates(subset=["source", "target"]) # TODO edges` can be empty
+
+    #no data from database, return from here
+    # TO-DO Front end response to be handled
+    if edges.empty:
+        return Response(json.dumps([]), mimetype="application/json")
+
+    #Timer to evaluate runtime between cypher-shell and extracting data
+    t_parsing = time.time()
+    print("Time Spent (Parsing):", t_parsing-t_neo4j)
+
 
     # Functional enrichment
     external_ids = nodes["external_id"].tolist()
@@ -108,6 +183,11 @@ def proteins_subgraph_api():
                 name=row["description"],
                 p_value=row["p_value"]
             ))
+
+    #Timer to evaluate enrichments runtime
+    t_enrich = time.time()
+    print("Time Spent (Enrichment):", t_enrich-t_parsing)
+
 
     if len(nodes.index) == 0:
         sigmajs_data = {
@@ -131,6 +211,10 @@ def proteins_subgraph_api():
         edgedf, nodedf = graph_utilities.create_graphdf(sigmajs_data)
         clusterdf = graph_utilities.generate_clusters(edgedf, nodedf)
         newCoordinates = graph_utilities.adjust_points(clusterdf)
+    
+    #Timer to evaluate runtime of calling gephi
+    t_gephi = time.time()
+    print("Time Spent (Gephi):", t_gephi-t_enrich)
 
     for node in sigmajs_data["nodes"]:
         df_node = nodes[nodes["id"] == int(node["id"])].iloc[0]
@@ -144,9 +228,14 @@ def proteins_subgraph_api():
 
     sigmajs_data["enrichment"] = list_enrichment
 
+    #Timer for final steps
+    t_end = time.time()
+    print("Time Spent (End):", t_end-t_gephi)
+
     json_str = json.dumps(sigmajs_data)
 
     return Response(json_str, mimetype="application/json")
+
 
 if __name__ == "__main__":
     app.run(
