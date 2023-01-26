e0b7d2558e006a10fb8fbc225d47db233b7e6cc9
Eulenspiege1 <theisner@posteo.de>
2023-01-26 18:14:16 +0100

    renamed scrip.py to edit_exp_data.py and improved filter.py

--- build_db/build_on_key_gene_name/python_files/filter.py
+++ build_db/build_on_key_gene_name/python_files/filter.py
@@ -40,31 +40,32 @@ def filter_for_individual_rows(file1: str, file2: str, key1: str,key2: str)\
     return(df2_filtered)
 
 
-def filter_for_same_rows(file1: str, file2: str, key1: str,key2: str)\
-    -> pd.DataFrame:
+def filter_for_rows(file1: pd.DataFrame, file2: pd.DataFrame, key1: str,
+                     key2: str, match: bool) -> pd.DataFrame:
     """
-    Input: 2 csv files and the key on which to filter on
-    Function: filter file2 for rows which are present in file 1
+    Takes 2 csv files, filters the rows of the second file in which 
+    the value of a column are the same/not the same as in the first one 
+    key1, key2: determines the columns on which to filter
+    match: determines if u look for same vlaues or different values
     Output: pd.Dataframe
     """
-    # read in the two csv files
-    df1 = pd.read_csv(file1)
-    df2 = pd.read_csv(file2)
-
     # specify the columns to use as the keys for filtering
     key_column_1 = key1
     key_column_2 = key2
 
     # set the columns to lower letters, to make the match case insensitive
-    df1[key_column_1] = df1[key_column_1].str.lower()    
-    df2[key_column_2] = df2[key_column_2].str.lower()
+    file1[key_column_1] = file1[key_column_1].str.lower()    
+    file2[key_column_2] = file2[key_column_2].str.lower()
     
     # create a set of the values in the key column for df1
-    df1_keys = set(df1[key_column_1])
+    df1_keys = set(file1[key_column_1])
 
-    # filter df2 for rows where the value in the key column
-    # is not in the df1_keys set
-    df2_filtered = df2[df2[key_column_2].isin(df1_keys)]
+    if match:
+        # filter for the equal value
+        df2_filtered = file2[file2[key_column_2].isin(df1_keys)]
+    else: 
+       # filter for the unequal value
+        df2_filtered = file2[~file2[key_column_2].isin(df1_keys)]
 
     # return the filtered rows
     return(df2_filtered)
@@ -94,7 +95,8 @@ if __name__ == '__main__':
     # define the file names, which get used
     files = ['string_proteins.csv','exp_de_proteins.csv']
     # define the file names to which gets written
-    target = ['string_proteins_filtered.csv','exp_de_proteins_filtered.csv']
+    target = ['string_proteins_filtered.csv','exp_de_proteins_filtered.csv',
+              'duplicate_proteins.csv']
     # Construct the file paths
     path_to_string_proteins = os.path.join(current_dir, files[0])
     path_to_exp_de_proteins = os.path.join(current_dir, files[1])
@@ -107,9 +109,15 @@ if __name__ == '__main__':
     # filter for rows where prefered_name does not start with ENSEMBL
     exp_de_proteins_filtered = delete_rows_having_value\
         (path_to_exp_de_proteins, 'SYMBOL', 'ENSMUSG')
+    # filter for proteins in exp_proteins which are present in string_proteins
+    proteins_duplicates = filter_for_rows(string_proteins_filtered_2,
+                                               exp_de_proteins_filtered,
+                                               'preferred_name',
+                                               'SYMBOL',True)
     # write the results to a csv
-    string_proteins_filtered_2.to_csv(target[0], index= False, na_rep='NA') 
-    exp_de_proteins_filtered.to_csv(target[1], index= False, na_rep='NA') 
+    #string_proteins_filtered_2.to_csv(target[0], index= False, na_rep='NA') 
+    #exp_de_proteins_filtered.to_csv(target[1], index= False, na_rep='NA')
+    proteins_duplicates.to_csv(target[2], index= False, na_rep='NA')
 
 
     
