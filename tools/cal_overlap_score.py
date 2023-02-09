942234bada423b27944a8e0d724b1637bdcd61f7
dyusuf <dilmurat.yusuf@gmail.com>
2023-02-09 12:11:23 +0100

    make a new method to calculate the ovelap sta between funcional terms
    the previous method is not feasible in terms of memory consumption
    when dealing with a large number (~20k) functional terms

--- tools/cal_overlap_score.py
+++ tools/cal_overlap_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 """
-A script to calculate the pairwise overlap score of elemets in a specific
-column of a DataFrame.
+A script to calculate the score of pairwise protein overlap between
+functional terms
 """
 
 __author__ = "Dilmurat Yusuf"
@@ -10,75 +10,64 @@ __author__ = "Dilmurat Yusuf"
 import itertools
 import pandas as pd
 import numpy as np
+import multiprocessing
+import gzip
 import time
+
+def cal_overlap_score(id1, id2, set1, set2, size1, size2):
+    """
+    calculate the overlap between two lists measured by
+    the fraction of smaller list that corresponds to
+    the size of overlap
+    """
+
+    # calculate the size of intersection
+    intersection_size = len(set1 & set2)
+
+    # the sizes are pre-calculated
+    # so when performing pairwise comparison of a long list
+    # len() won't be repeated over the same list
+    min_set = min(size1, size2)
+
+    return id1, id2, intersection_size / min_set
+
+def cal_overlap_score_worker(args):
+    """
+    for multiprocessing
+    """
+
+    i, j = args
+    score = cal_overlap_score(df.loc[i].external_id,
+                              df.loc[j].external_id,
+                              df.loc[i].proteins,
+                              df.loc[j].proteins,
+                              df.loc[i].sizes,
+                              df.loc[j].sizes)
+    return f"{score[0]},{score[1]},{score[2]}\n"
 
 start_time = time.time()
 
 target_file = '../data/MusMusculusDATA/TermsWithProteins.csv.gz'
-#t df = pd.DataFrame({'proteins': [[1, 2 , 'c'], [1, 2, 3, 4], [1, 0, 'a', 4, 5]], 'external_id': ['a','b','c']})
 df = pd.read_csv(target_file, compression='gzip')
-df['proteins'] = df['proteins'].apply(eval)
+df['proteins'] = df['proteins'].apply(eval).apply(set)
 df = df[['external_id', 'proteins']]
+#t df = pd.DataFrame({'proteins': [{1, 2 , 'c'}, {1, 2, 3, 'd'}, {1, 2, 3, 'a', 'b'}], 'external_id': ['a','b','c']})
 
-#--- get the smaller set in a pairwise comparison
 # calculate the sizes of each element
-sizes = df['proteins'].apply(len).values
-# list of pairwise comparison
-paired_comp = [','.join(sorted((df['external_id'][tags[0]], df['external_id'][tags[1]]))) for tags in itertools.combinations(range(len(sizes)), 2)]
-# pick the one with minimum size in a pairwise comparison
-paired_min = [min(eles) for eles in itertools.combinations(sizes, 2)]
-list_of_tuples = list(zip(paired_comp, paired_min))
-df_min = pd.DataFrame(list_of_tuples, columns=['pair', 'min_len'])
-del paired_comp, paired_min, list_of_tuples
-#---
+# this will be used later when calcualted overlap score
+df["sizes"] = [len(ele) for ele in df['proteins']]
 
-#--- get the size of intersection
-# Convert to set
-a = df['proteins'].apply(set).values
-# Broadcast set intersection
-new_df = pd.DataFrame(a[:, None] & a)
-new_df.index = df['external_id'].values
-new_df.columns = df['external_id'].values
+score_file = "../data/MusMusculusDATA/functional_terms_overlap.csv.gz"
+start_time = time.time()
+with gzip.open(score_file, "wb") as f:
+    pool = multiprocessing.Pool()
+    positions = list(itertools.combinations(range(len(df)), 2))
+    results = pool.map(cal_overlap_score_worker, positions)
+    f.write("".join(results).encode())
+print(f'completed overlap calculation and saved in {score_file}')
 
-
-# Extract the upper triangular elements
-mask = np.triu(np.ones_like(new_df), k=1).astype(bool)
-upper_triangular = new_df.where(mask)
-del new_df
-# Convert the extracted elements to long form
-upper_triangular = upper_triangular.unstack().reset_index()
-upper_triangular.columns = ["row", "col", "value"]
-upper_triangular = upper_triangular[~upper_triangular["value"].isnull()]
-upper_triangular['inters_len'] = upper_triangular['value'].apply(len)
-upper_triangular['pair'] = [','.join(sorted(eles)) for eles in zip(upper_triangular.row, upper_triangular.col)]
-#---
-
-#---
-# merge the two data sets
-df_min_inters = pd.merge(df_min, upper_triangular[['pair', 'inters_len']],
-                           on = "pair",
-                           how = "outer")
-del df_min, upper_triangular
-df_min_inters["overlap_score"] = df_min_inters["inters_len"]/df_min_inters["min_len"]
-df_min_inters = df_min_inters[['pair', 'overlap_score']]
-df_min_inters.set_index(['pair']).apply(pd.Series.explode).reset_index()
-
-# Split the values in the 'pair' column
-df_exploded = df_min_inters['pair'].str.split(',', expand=True)
-
-# Add a new column for each split result
-df_exploded['ID_1st'] = df_exploded[0]
-df_exploded['ID_2nd'] = df_exploded[1]
-
-# Drop the original 'pair' column and the intermediate split result
-df_min_inters = df_min_inters.drop('pair', axis=1)
-df_exploded = df_exploded.drop([0, 1], axis=1)
-
-# Join the exploded DataFrame with the original DataFrame
-df_min_inters = df_exploded.join(df_min_inters)
-
-df_min_inters.to_csv('../data/MusMusculusDATA/functional_terms_overlap.csv', index=False)
-print('completed')
-#---
-# !less ../data/MusMusculusDATA/functional_terms_overlap.csv
-print("--- %s seconds ---" % (time.time() - start_time))
+time_cost = (time.time() - start_time)/3600
+time_file = "../data/MusMusculusDATA/functional_terms_overlap_time_cost.txt"
+with open(time_file, "w") as f:
+    f.write(f'time cost: {time_cost} hours\n')
+print(f'time cost at {time_file}')
