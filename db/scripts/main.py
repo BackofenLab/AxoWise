9e80e16341344752548011cf3a57f05b05779c89
Vincent Kataikko <vincent@kataikko.de>
2023-06-12 12:06:11 +0200

    Added basic upload functionality

--- db/scripts/main.py
+++ db/scripts/main.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-import read
+import read as rd
+from upload import extend_db_from_experiment
 
 _DEFAULT_EXPERIMENT_PATH = Path("../source/experiment")
 _DEFAULT_STRING_PATH = Path("../source/string")
@@ -7,16 +8,19 @@ _DEFAULT_FUNCTIONAL_PATH = Path("../source/functional")
 
 
 def read_experiment_files(path=_DEFAULT_EXPERIMENT_PATH):
-    data = read.parse_experiment(dir_path=path, reformat=True)
+    data = rd.parse_experiment(dir_path=path, reformat=True)
     return data
 
 def read_string_files(path=_DEFAULT_STRING_PATH):
-    data = read.read_string(dir_path=path)
+    data = rd.parse_string(dir_path=path)
     return data
 
 def read_functional_files(path=_DEFAULT_FUNCTIONAL_PATH):
-    data = read.read_functional(dir_path=path)
+    data = rd.parse_functional(dir_path=path)
     return data
 
 if __name__ == "__main__":
-    read_experiment_files()
+    tg_nodes, tf_nodes, de_values, or_nodes, da_values, tf_tg_corr, tf_or_corr, motif = read_experiment_files()
+    
+    extend_db_from_experiment(tg_nodes=tg_nodes, tf_nodes=tf_nodes, or_nodes=or_nodes)
+
