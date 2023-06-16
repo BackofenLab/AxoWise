cf9836ccd3d26a343f7c196610927bca2b832688
Alken0 <54180229+Alken0@users.noreply.github.com>
2023-06-16 12:57:05 +0200

    NN-218 prevent cypher-injections

--- backend/src/queries.py
+++ backend/src/queries.py
@@ -9,64 +9,78 @@ import neo4j
 
 def get_terms_connected_by_kappa(driver: neo4j.Driver, term_ids: list[str]):
     """:returns: terms, source, target, score"""
+    parameters = {
+        "term_ids": term_ids
+    }
+    query = f"""
+                MATCH (source:Terms)-[association:KAPPA]->(target:Terms)
+                WHERE source.external_id IN $term_ids 
+                AND target.external_id IN $term_ids
+                RETURN source, target, association.score AS score;
+                """
     with driver.session() as session:
-        query = f"""
-            MATCH (source:Terms)-[association:KAPPA]->(target:Terms)
-            WHERE source.external_id IN {term_ids} 
-            AND target.external_id IN {term_ids}
-            RETURN source, target, association.score AS score;
-            """
-        result = session.run(query)
+        result = session.run(query, parameters)
         return _convert_to_connection_info_float_score(result)
 
 
 def get_protein_ids_for_names(driver: neo4j.Driver, names: list[str], species_id: int) -> list[str]:
+    parameters = {
+        "species_id": species_id,
+        "names": [n.upper() for n in names]
+    }
     query = f"""
         MATCH (protein:Protein)
-        WHERE protein.species_id = {species_id}
-        AND protein.name IN {[n.upper() for n in names]} 
+        WHERE protein.species_id = $species_id
+        AND protein.name IN $names 
         RETURN protein.external_id AS id
     """
     with driver.session() as session:
-        result = session.run(query)
+        result = session.run(query, parameters)
         return [x["id"] for x in list(result)]
 
 
 def get_protein_neighbours(
-    driver: neo4j.Driver, protein_ids: list[str], threshold: int
+        driver: neo4j.Driver, protein_ids: list[str], threshold: int
 ) -> (list[str], list[str], list[str], list[int]):
     """
     :returns: proteins, source, target, score
     """
-
+    parameters = {
+        "protein_ids": protein_ids,
+        "threshold": threshold
+    }
     query = f"""
         MATCH (source:Protein)-[association:ASSOCIATION]-(target:Protein)
-        WHERE source.external_id IN {protein_ids} 
-        OR target.external_id IN {protein_ids} 
-        AND association.combined >= {threshold} 
+        WHERE source.external_id IN $protein_ids
+        OR target.external_id IN $protein_ids
+        AND association.combined >= $threshold
         RETURN source, target, association.combined AS score
     """
     with driver.session() as session:
-        result = session.run(query)
+        result = session.run(query, parameters)
         return _convert_to_connection_info_int_score(result)
 
 
 def get_protein_associations(
-    driver: neo4j.Driver, protein_ids: list[str], threshold: int
+        driver: neo4j.Driver, protein_ids: list[str], threshold: int
 ) -> (list[str], list[str], list[str], list[int]):
     """
     :returns: proteins, source, target, score
     """
+    parameters = {
+        "protein_ids": protein_ids,
+        "threshold": threshold
+    }
     query = f"""
         MATCH (source:Protein)-[association:ASSOCIATION]->(target:Protein)
-        WHERE source.external_id IN {protein_ids}
-        AND target.external_id IN {protein_ids}
-        AND association.combined >= {threshold}
+        WHERE source.external_id IN $protein_ids
+        AND target.external_id IN $protein_ids
+        AND association.combined >= $threshold
         RETURN source, target, association.combined AS score
     """
 
     with driver.session() as session:
-        result = session.run(query)
+        result = session.run(query, parameters)
         return _convert_to_connection_info_int_score(result)
 
 
