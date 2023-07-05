2eee969ea3e6ed31a47e9f7e8b800464600b1457
Vincent Kataikko <vincent@kataikko.de>
2023-07-05 10:38:38 +0200

    lint changes

--- db/scripts/querier.py
+++ db/scripts/querier.py
@@ -15,14 +15,18 @@ def run_queries():
     driver = start_driver()
 
     test_genes = list(pd.read_csv("../source/query_testing/genes.csv")["SYMBOL"])
-    
+
     # Queries
     gene_subset = get_tg_ensembl_by_symbol(gene_list=test_genes, driver=driver)
     or_subset = [i[0] for i in get_or_by_distance_to_tg(subset=gene_subset, driver=driver)]
-    get_or_by_da_under_contexts(contexts=["12h-0h", "24h-0h"], subset=or_subset, positive=True, threshold=0.5, driver=driver)
+    get_or_by_da_under_contexts(
+        contexts=["12h-0h", "24h-0h"], subset=or_subset, positive=True, threshold=0.5, driver=driver
+    )
     get_or_by_motif_to_tf(tf="ENSMUSG00000052684", subset=or_subset, driver=driver)
     get_tg_by_correlation_tf(tf="ENSMUSG00000052684", subset=gene_subset, positive=True, threshold=0.5, driver=driver)
-    get_tg_by_de_under_contexts(contexts=["6h-0h", "24h-0h"], subset=gene_subset, positive=True, threshold=0.5, driver=driver)
+    get_tg_by_de_under_contexts(
+        contexts=["6h-0h", "24h-0h"], subset=gene_subset, positive=True, threshold=0.5, driver=driver
+    )
     get_tg_by_link_ft(ft="GO:0070851", subset=gene_subset, driver=driver)
 
     stop_driver(driver=driver)
