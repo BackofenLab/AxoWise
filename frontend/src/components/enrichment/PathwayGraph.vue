00c2aedf7d963d5f3445ad041b33482f60758958
TripZz <nico.greiner9@gmail.com>
2023-09-13 13:20:59 +0200

    NN-340 Implemented the base construct of pathway graphs overview - Implement the generation of new pathway graphs - Implement the naming of individual pathway graphs - Implement the favorite system for pathway graphs - Implement snapshot image of individual pathway graph

--- frontend/src/components/enrichment/PathwayGraph.vue
+++ frontend/src/components/enrichment/PathwayGraph.vue
@@ -1,15 +1,84 @@
 <template>
     <div id="pathways-graphs">
-        <img src="@/assets/pathwaybar/pathway-button.png">
-        <div class="graph-section"></div>
+        <div class="generate-graph">
+            <div class="generate-text" v-on:click="get_term_data()">Generate term graph</div>
+        </div>
+        <div class="graph-section">
+            <div class="slider" tabindex="0">
+                <div v-for="(entry, index) in term_graphs" :key="index" class="graph" v-on:click="switch_graph()" @mouseover="activeGraphIndex = index" @mouseout="activeGraphIndex = -1">
+                    <SnapshotGraph :propValue="entry" :index="entry.id"/>
+                    <div class="graph-options" v-show="activeGraphIndex == index" >
+                        <div class="bookmark-graph" v-on:click.stop="add_graph(entry)" :class="{ checked: favourite_graphs.has(entry)}" ref="checkboxStatesGraph"></div>
+                        <img  class="remove-graph" src="@/assets/pathwaybar/cross.png" v-on:click.stop="remove_graph(entry, index)">
+                        <div class="graph-name">
+                            <input type="text" v-model="entry.label" class="empty" @click.stop />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 
+import SnapshotGraph from '@/components/enrichment/SnapshotGraph.vue'
+
 export default {
     name: 'PathwayGraphs',
-    
+    props: ['terms'],
+    components: {
+        SnapshotGraph,
+    },
+    data() {
+        return {
+            api: {
+                termgraph: "api/subgraph/terms",
+            },
+            term_graphs: new Set(),
+            favourite_graphs: new Set(),
+            activeGraphIndex: -1,
+            graph_number: 0
+        }
+    },
+    methods: {
+        get_term_data() {
+            var com = this
+
+            var formData = new FormData()
+            formData.append('func-terms', JSON.stringify(com.terms))
+
+            this.axios
+                .post(com.api.termgraph, formData)
+                .then((response) => {
+                    this.$store.commit('assign_term_graph', response.data)
+                    this.graph_number += 1
+                    this.$store.commit('assign_new_term_graph', {label: `Graph ${this.graph_number}`, graph: response.data})
+                    this.term_graphs.add({ id: this.graph_number, label: `Graph ${this.graph_number}`, graph: response.data});
+                })
+
+        },
+        switch_graph() {
+            this.$router.push("terms")
+        },
+        remove_graph(entry, index) {
+            if (!this.favourite_graphs.has(entry)) {
+                // Checkbox is checked, add its state to the object
+                this.favourite_graphs.delete(entry)
+            }
+            this.term_graphs.delete(entry)
+            this.$store.commit('remove_snapshotPathway', index)
+        },
+        add_graph(entry){
+            if (!this.favourite_graphs.has(entry)) {
+                // Checkbox is checked, add its state to the object
+                this.favourite_graphs.add(entry)
+            } else {
+                // Checkbox is unchecked, remove its state from the object
+                this.favourite_graphs.delete(entry)
+            }
+        },
+    }
 }
 </script>
 
@@ -24,18 +93,114 @@ export default {
         margin-left: 48.74%;
         border-radius: 10px;
         z-index: 999;
+        font-family: 'ABeeZee', sans-serif;
     }
-    #pathways-graphs img {
+    .generate-graph {
         width: 24.20%;
         height: 11.16%;
         position: absolute;
+        border-radius: 5px;
+        background: #0A0A1A;
+        cursor: default;
     }
+    .generate-graph .generate-text {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color: white;
+        font-size: 0.95vw;
+    }
+
     .graph-section {
         width: 100%;
         height: 87.65%;
         top: 12.35%;
+        display: flex;
         border-radius: 10px;
         background: #0A0A1A;
         position: absolute;
+        justify-content: center;
+    }
+
+    .graph-section .slider {
+        position: absolute;
+        width: 90.78%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        overflow-x: scroll;
+        scroll-behavior: smooth;
+        scroll-snap-type: x mandatory;
+
+    }
+
+    .graph-section .slider .graph{
+        position: relative;
+        width: 31.4%;
+        height: 71.71%;
+        flex-shrink: 0;
+        margin: 0% 1% 0% 1%;
+        border-radius: 5px;
+        border: 1px solid #FFF;
+        background: rgba(217, 217, 217, 0.12);
+        transform-origin: center center;
+        transform: scale(1);
+        scroll-snap-align: center;
+        display: flex;
+    }
+
+    .bookmark-graph {
+        display: block;
+        width: 0.9vw;
+        height: 0.9vw;
+        margin: 1% 1% 0 0;
+        background-color: white;
+        -webkit-mask: url(@/assets/pathwaybar/star-solid.svg) no-repeat center;
+        mask: url(@/assets/pathwaybar/star-solid.svg) no-repeat center;
+        mask-size: 0.9vw;
+        background-repeat: no-repeat;
+    }
+
+    .remove-graph {
+        width: 0.9vw;
+        height: 0.9vw;
+        -webkit-filter: invert(100%); /* Safari/Chrome */
+        filter: invert(100%);
+        margin: 1% 1% 0 0;
+    }
+
+    .graph-options {
+        position: fixed;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: end;
+    }
+
+    .graph-name {
+        position: fixed;
+        display: flex;
+        bottom: 5%;
+        width: 100%;
+        height: 20%;
+        border-radius: 0 0 5px 5px;
+        background-color: #0A0A1A;
+        text-align-last: center;
+        justify-content: center;
+    }
+
+    .graph-name input[type=text] {
+        font-size: 0.85vw;
+        background: none;
+        color: white;
+        cursor: default;
+        font-family: 'ABeeZee', sans-serif;
+        border: none;
+    }
+
+    .checked {
+        background-color: #ffa500;
     }
 </style>
