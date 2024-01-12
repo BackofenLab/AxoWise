28ff454a9174aedef41a9572b524ea0e78411e68
TripZz <nico.greiner9@gmail.com>
2024-01-12 11:03:28 +0100

    NN-427 Added draggable option to pathway pane

--- frontend/src/components/enrichment/PathwayMenu.vue
+++ frontend/src/components/enrichment/PathwayMenu.vue
@@ -1,7 +1,7 @@
 <template>
-    <div :class="{ 'pathwaybar-small': pane_hidden === true, 'pathways': pane_hidden === false }">
-        <div class="minimize-bar" v-on:click="pane_hidden = !pane_hidden" >
-            <img src="@/assets/pathwaybar/fullscreen.png">
+    <div id="menu-tools" :class="{ 'pathwaybar-small': pane_hidden === true, 'pathways': pane_hidden === false }">
+        <div id="menu-tools_header">
+            <img src="@/assets/pathwaybar/fullscreen.png" v-on:click="pane_hidden = !pane_hidden">
         </div>
         <div class="pathwaybar">
             <PathwayList
@@ -36,7 +36,7 @@ export default {
     components: {
         PathwayList,
         PathwayTools,
-        PathwaySet
+        PathwaySet,
     },
     data() {
         return {
@@ -58,6 +58,12 @@ export default {
             },
             deep: true
         },
+        pane_hidden(val) {
+            if (val) this.dragElement(document.getElementById("menu-tools"),val);
+            else {
+                this.resetElement(document.getElementById("menu-tools"));
+            }
+        }
     },
     mounted() {
         var com = this
@@ -88,9 +94,57 @@ export default {
 
         },
         abort_enrichment() {
-                this.sourceToken.cancel('Request canceled');
-                this.await_load = false 
-            },
+            this.sourceToken.cancel('Request canceled');
+            this.await_load = false 
+        },
+        dragElement(elmnt, val) {
+            var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+            if (document.getElementById(elmnt.id + "_header")) {
+                // if present, the header is where you move the DIV from:
+                document.getElementById(elmnt.id + "_header").onmousedown = dragMouseDown;
+            } else {
+                // otherwise, move the DIV from anywhere inside the DIV: 
+                elmnt.onmousedown = dragMouseDown;
+            }
+
+            function dragMouseDown(e) {
+                e = e || window.event;
+                e.preventDefault();
+                // get the mouse cursor position at startup:
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                document.onmouseup = closeDragElement;
+                // call a function whenever the cursor moves:
+                document.onmousemove = elementDrag;
+            }
+
+            function elementDrag(e) {
+                if (!val) return
+                e = e || window.event;
+                e.preventDefault();
+                // calculate the new cursor position:
+                pos1 = pos3 - e.clientX;
+                pos2 = pos4 - e.clientY;
+                pos3 = e.clientX;
+                pos4 = e.clientY;
+                // set the element's new position:
+                elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+                elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+            }
+
+            
+            function closeDragElement() {
+                // stop moving when mouse button is released:
+                document.onmouseup = null;
+                document.onmousemove = null;
+            }
+        },
+        resetElement(elmnt) {
+            // set the element's new position:
+            elmnt.style.top = null;
+            elmnt.style.left = null;
+            document.getElementById(elmnt.id + "_header").onmousedown = null;
+        }
     },
     created() {
 
@@ -116,7 +170,7 @@ export default {
         transform: translateX(-50%);
         transition: transform 330ms ease-in-out;
     }
-    .minimize-bar {
+    #menu-tools_header {
         width: 100%;
         height: 9.41%;
         position: absolute;
@@ -127,7 +181,7 @@ export default {
         transition: transform 330ms ease-in-out;
     }
 
-    .minimize-bar img{
+    #menu-tools_header img{
         position: absolute;
         top: 35%;
         right: 0.7vw;
