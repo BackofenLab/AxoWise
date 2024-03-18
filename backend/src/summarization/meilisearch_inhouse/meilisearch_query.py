7f0dc8ac02394d36ed384c8b97e546935f162d99
Maluuck <mwarshafani1@gmail.com>
2024-03-18 12:07:08 +0100

    NN-432 Change meilisearch structure

---
+++ backend/src/summarization/meilisearch_inhouse/meilisearch_query.py
@@ -0,0 +1,107 @@
+import meilisearch
+import json
+from meilisearch_inhouse import Api_key
+
+
+def build_query(client, index, user_input, sort_by, limit=20000):
+    """
+    Function to search meilisearch, returns results.json file
+
+    User/frontend supplies the query and index - hardcoded index atm
+
+    Build 2 queries if one of the symbols has a full name
+
+    """
+
+    # Dict full name links symbols to their full names
+    with open("summarization/meilisearch_inhouse/Dict_full_name.json", "r", encoding="utf-8") as json_file:
+        full_name = json.load(json_file)
+
+    found_full_name = False
+    symbol_query = ""
+    full_name_query = ""
+
+    user_input = user_input.split()
+
+    for x in user_input:
+        # check if the word x is an symbol / alias, if yes save full name in second query
+        # if not just save the normal symbol to the query
+        if x.lower() in full_name:
+            full_name_query += ' "' + full_name[x.lower()] + '" '
+            found_full_name = True
+
+            # remove '-' in the input, workaround il-10 --> il10 to avoid results  containing only 10 or il
+            # with il10 we still get il-10 as result, but with query il-10 we also get il-9, il-8,.....
+            x = x.replace("-", "")
+            x = x.replace(",", " ")
+            symbol_query += " " + x + " "
+
+        else:
+            # remove '-' in the input, workaround il-10 --> il10 to avoid results  containing only 10 or il
+            # with il10 we still get il-10 as result, but with query il-10 we also get il-9, il-8,.....
+            x = x.replace("-", "")
+            x = x.replace(",", " ")
+
+            # weird behaviour, withous this the query doesnt work as expected
+            # ("il10 cancer stroke") != ("il10 stroke cancer")
+            symbol_query += " " + x + " "
+            full_name_query += " " + x + " "
+
+    result_symbol = search(client, index, symbol_query, sort_by, limit)
+
+    if found_full_name:
+        result_full_name = search(client, index, full_name_query, sort_by, limit)
+
+        # add the results from full name to symbol results
+        result_symbol["hits"].extend(result_full_name["hits"])
+        result_symbol["processingTimeMs"] += result_full_name["processingTimeMs"]
+
+    return result_symbol
+
+
+def search(client, index, query, sort_by, limit):
+    """
+    Sends the query to meilisearch
+
+    Parameters:
+        client: established connection to meilisearch
+        index: index to be searched
+        query: Query to be searched
+    """
+
+    result = client.index(index).search(
+        query,
+        {
+            "limit": limit,  # returns a maximum of limit results - make sure limit < = max variable
+            "matchingStrategy": "all",  # results have to match all words in query or their synonyms
+            "sort": sort_by,
+        },
+    )
+
+    return result
+
+
+def save_results(data, filename):
+    """
+    Saves data to file
+    """
+
+    with open(filename, "w", encoding="utf-8") as outfile:
+        json.dump(data, outfile, indent=2)
+    print(f"Find results in {filename}")
+
+
+def get_results(
+    limit,
+    query,
+):
+    """
+    This function is just here until the connection to front end it established
+    """
+    # TODO:
+    # This needs to be moved to the main function when connecting to the frontend
+    # import Api_key
+    client = meilisearch.Client("http://localhost:7700", Api_key.SEARCH_KEY)
+    index = "pubmed_v1"
+    sort_by = ["Cited number:desc", "Published:desc"]
+    return build_query(client, index, query, sort_by, limit)
---
+++ backend/src/summarization/meilisearch_inhouse/update_config.py
@@ -0,0 +1,51 @@
+import meilisearch
+import Api_key
+import time
+
+
+def main():
+    client = meilisearch.Client("http://localhost:7700", Api_key.MASTER_KEY)
+    index = str(input("Which index do you want to update? "))
+    limit = int(input("What should the new limit be? "))
+
+    # set a limit for the index - 20k is good enough
+    task = client.index(index).update_pagination_settings({"maxTotalHits": limit})
+    print(task.task_uid)
+
+    # disable typo allowance
+    client.index(index).update_typo_tolerance({"enabled": False})
+
+    # add Cited number and Published to the sortable attributes
+    task = client.index(index).update_sortable_attributes(["Cited number", "Published"])
+    check_status(client, task)
+
+    # update the ranking rules --> moved sort higher up
+    task = client.index(index).update_ranking_rules(["words", "sort", "typo", "proximity", "attribute", "exactness"])
+    check_status(client, task)
+
+    print("Settings updated")
+
+
+def check_status(client, task):
+    """
+    tracks the status of the task and only returns when the task failed or is finished
+
+    Needed atm to not spam the server with too many tasks -> might not be needed in a future
+    version of meilisearch
+    """
+
+    task_id = task.task_uid
+
+    while task.status == "enqueued" or task.status == "processing":
+        time.sleep(2)  # we use sleep to avoid spamming the server with status requests
+        task = client.get_task(task_id)
+
+    if task.status == "failed":
+        print(f"Failed to process task {task_id}, check meilisearch console")
+        return
+
+    return
+
+
+if __name__ == "__main__":
+    main()
