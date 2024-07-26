a3f74d58ec522f15ab9d922374fe965bb040e822
dyusuf <dilmurat.yusuf@gmail.com>
2024-07-26 10:05:01 +0000

    NN-527 Add URL scheme validation to ensure http or https
    - Parsed the URL using urlparse to check the scheme.
    - Added a validation to raise a ValueError if the URL scheme is not http or https.
    - Ensures only valid URL schemes are accepted, enhancing the robustness of the code.

--- backend/src/util/data_util.py
+++ backend/src/util/data_util.py
@@ -7,6 +7,7 @@ import functools
 import os
 import re
 import time
+from urllib.parse import urlparse
 from urllib.request import urlopen
 
 
@@ -104,9 +105,14 @@ def parse_flat_file(pathway):
 def get(url, timeout=10, wait=1):
     if type(wait) is int and wait > 0:
         time.sleep(wait)
+
+    parsed_url = urlparse(url)
+    if parsed_url.scheme not in ["http", "https"]:
+        raise ValueError("URL scheme must be http or https")
+
     try:
         return (
-            urlopen(url, timeout=timeout)
+            urlopen(url, timeout=timeout)  # nosec
             .read()
             .decode(encoding="utf-8", errors="ignore")
         )
--- scraping/KEGG/url.py
+++ scraping/KEGG/url.py
@@ -1,13 +1,19 @@
 import time
+from urllib.parse import urlparse
 from urllib.request import urlopen
 
 
 def get(url, timeout=10, wait=1):
     if type(wait) is int and wait > 0:
         time.sleep(wait)
+
+    parsed_url = urlparse(url)
+    if parsed_url.scheme not in ["http", "https"]:
+        raise ValueError("URL scheme must be http or https")
+
     try:
         return (
-            urlopen(url, timeout=timeout)
+            urlopen(url, timeout=timeout)  # nosec
             .read()
             .decode(encoding="utf-8", errors="ignore")
         )
