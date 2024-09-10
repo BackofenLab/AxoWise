88d672ec702f385b2f8ef61d7fe6af531f5c8704
Maluuck <mwarshafani1@gmail.com>
2024-09-10 18:39:38 +0200

    NN-581 use embed_documents for parallelization

--- backend/src/generate_embeddings.py
+++ backend/src/generate_embeddings.py
@@ -1,3 +1,5 @@
+import time
+
 import pandas as pd
 from langchain_ollama import OllamaEmbeddings
 
@@ -7,24 +9,27 @@ embedder = OllamaEmbeddings(model="llama3.1")
 # Step 1: Read the CSV into a DataFrame
 df = pd.read_csv("/mnt/workspace/for_graph_RAG/PMID_abstracts.csv")
 
-# Step 2: Define the chunk size
+# Step 2: Define the chunk size (100,000 rows per CSV file)
 chunk_size = 500000
 total_rows = len(df)
 
 # Step 3: Loop through the DataFrame in chunks of 100,000 rows
 for i in range(0, total_rows, chunk_size):
+    time_begin = time.time()
     # Get the chunk of data
     df_chunk = df[i : i + chunk_size].copy()
 
-    # Step 4: Generate embeddings for each abstract in the chunk
-    df_chunk["embeddings"] = df_chunk["abstract"].apply(
-        lambda x: embedder.embed_query(x)
-    )
+    print(f"Started generating embeddings of chunk {i//chunk_size + 1}")
 
+    # Step 4: Generate embeddings for each abstract in the chunk
+    embeddings = embedder.embed_documents(list(df_chunk["abstract"]))
+    df_chunk["embeddings"] = embeddings
     # Step 5: Save each chunk as a separate CSV file
     chunk_file_name = f"genes_abstract_with_embeddings_part_{i//chunk_size + 1}.gz"
     df_chunk.to_csv(chunk_file_name, index=False)
 
-    print(f"Saved {chunk_file_name} with {len(df_chunk)} rows.")
+    print(
+        f"Saved {chunk_file_name} with {len(df_chunk)} rows in {time.time()-time_begin}s."
+    )
 
 print("Splitting and embedding generation completed.")
