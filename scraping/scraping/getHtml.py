a74265023f5cc2aed042420bc9bf5d7eba8335e6
Momina Atif Dar <p180030@nu.edu.pk>
2024-10-31 14:15:47 +0100

    NN-601 Automate scraping source code from Journals

---
+++ scraping/scraping/getHtml.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
+import re
+
+def setup_chrome_driver():
+    """set up Chrome driver with specific Chrome installation"""
+    chrome_options = Options()
+    chrome_options.add_argument('--headless') 
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    chrome_options.binary_location = r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+
+    chrome_service = Service(
+        executable_path=r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe"
+    )
+
+    return webdriver.Chrome(service=chrome_service, options=chrome_options)
+
+def scrape_publications():
+    """scrapes the full HTML content with infinite scroll"""
+
+    base_url = "https://www.10xgenomics.com/publications?refinementList%5Bspecies%5D%5B0%5D=Human&page=1"
+    
+    driver = setup_chrome_driver()
+
+    try:
+        driver.get(base_url)
+
+        last_height = driver.execute_script("return document.body.scrollHeight")    # to get the initial height of page
+
+        while True:
+
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")    # scroll down to the bottom
+
+            time.sleep(1)
+
+            new_height = driver.execute_script("return document.body.scrollHeight") # height of new page with more content
+
+            publication_elements = driver.find_element(By.CLASS_NAME, "PublicationSearch")  # find and extract HTML
+            html_content = publication_elements.get_attribute("outerHTML")
+
+            with open("publications_humans.html", "w", encoding="utf-8") as f:
+                f.write(html_content + "\n")
+
+            if new_height == last_height:   # no change in height, all content is loaded
+                print("Reached the end of the page.")
+                break
+            last_height = new_height
+
+    finally:
+        driver.quit()
+
+def get_doi():
+    """extracts DOI URLs from .html"""
+
+    url = r"./publications_humans.html"
+    with open(url, "r", encoding="utf-8") as f:
+        soup = BeautifulSoup(f, 'html.parser')
+
+    searchHits = soup.find_all('a', {'class':'css-1nszd81 es4dp9v0'})   #class contains only DOI and PubMedID
+
+    with open("searchHits_humans.txt", "w", encoding="utf-8") as f2:
+        for hit in searchHits:
+            pattern = r'https://doi.org/[^\s"]+'    #pick only DOI hrefs
+            url = re.search(pattern, str(hit))
+
+            if url:
+                f2.write(str(url[0]) + "\n")
+
+
+if __name__ == "__main__":
+    scrape_publications()
+    get_doi()
