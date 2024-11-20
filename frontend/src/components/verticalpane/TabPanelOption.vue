f41ced7df169a515fdc448534dcf7cb30c96abff
anlisha-maharjan <anlishamaharjan@gmail.com>
2024-11-20 23:10:58 +0100

    NN-610 implement draggable tabs in vertical pane

---
+++ frontend/src/components/verticalpane/TabPanelOption.vue
@@ -0,0 +1,82 @@
+<template>
+  <TabPanels class="h-[calc(100%-34px)] flex flex-col !p-0 !px-3 overflow-auto">
+    <TabPanel class="flex-1" value="set">
+      <PathwaySet :gephi_data="gephi_data" :api="api" :mode="mode"></PathwaySet>
+    </TabPanel>
+
+    <TabPanel class="flex-1" value="layers">
+      <PathwayLayers :active_termlayers="active_termlayers" :gephi_data="gephi_data" />
+    </TabPanel>
+
+    <TabPanel class="flex-1" value="difexp" v-if="dcoloumns">
+      <DifExpMenu :active_decoloumn="active_decoloumn" :gephi_data="gephi_data" />
+    </TabPanel>
+
+    <TabPanel class="flex-1" value="list">
+      <PathwayList :gephi_data="gephi_data" :terms="terms" :await_load="await_load"
+        :favourite_pathways="favourite_pathways" 
+        @favourite_pathways_changed="favourite_pathways = $event"
+        @filtered_terms_changed="filtered_terms = $event"></PathwayList>
+    </TabPanel>
+
+    <TabPanel class="flex-1" value="graph">
+      <PathwayTools :gephi_data="gephi_data" :filtered_terms="filtered_terms" :favourite_pathways="favourite_pathways">
+      </PathwayTools>
+    </TabPanel>
+
+    <TabPanel class="flex-1" value="heatmap">
+      <HeatmapTool :gephi_data="gephi_data" :filtered_terms="filtered_terms" :favourite_pathways="favourite_pathways">
+      </HeatmapTool>
+    </TabPanel>
+
+    <TabPanel class="flex-1" value="citation">
+      <CitationMenu :active_node="active_node" :active_background="active_background"></CitationMenu>
+    </TabPanel>
+  </TabPanels>
+</template>
+
+<script>
+import PathwayList from "@/components/enrichment/PathwayList.vue";
+import PathwayTools from "@/components/enrichment/PathwayTools.vue";
+import HeatmapTool from "@/components/enrichment/HeatmapTool.vue";
+import PathwaySet from "@/components/enrichment/PathwaySet.vue";
+import CitationMenu from "@/components/citation/CitationMenu.vue";
+import PathwayLayers from "@/components/pane/modules/layer/PathwayLayers.vue";
+import DifExpMenu from "@/components/pane/modules/difexp/DifExpMenu.vue";
+
+export default {
+  name: "TabPanelOption",
+  props: ["mode", "modelValue", "gephi_data", "active_decoloumn", "active_node", "active_background", "active_termlayers", "terms", "api", "await_load"],
+  emits: ["update:modelValue"],
+  components: {
+    PathwayList,
+    PathwayTools,
+    PathwaySet,
+    HeatmapTool, 
+    PathwayLayers, 
+    DifExpMenu, 
+    CitationMenu
+  },
+  data() {
+    return {
+      dcoloumns: this.$store.state.dcoloumns,
+      favourite_pathways: [],
+      filtered_terms: [],
+    };
+  },
+  watch: {
+    favourite_pathways: {
+      handler(newList) {
+        this.emitter.emit("updateFavouriteList", newList);
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    setActiveTab(value) {
+      this.$emit("update:modelValue", value);
+    },
+  },
+  mounted() { },
+};
+</script>
--- frontend/src/components/verticalpane/VerticalPane.vue
+++ frontend/src/components/verticalpane/VerticalPane.vue
@@ -1,5 +1,60 @@
 <template>
-  <div id="vertical-pane">
+  <Card class="w-[26rem] !rounded-none"
+    :pt="{ body: { class: 'h-full !py-3 !px-0' }, content: { class: 'h-full flex flex-col gap-4' } }">
+    <template #content>
+      <section :class="`flex relative overflow-hidden transition duration-300 ease-in-out ${containerA.length > 0 ? 'flex-[1_1_50%]' : ''
+        }`">
+        <Tabs class="flex-1 w-full tab-active-grad" scrollable :value="active_tabA" @update:value="onchangeTabA">
+          <TabList :class="`flex-shrink-0 ${dragged_to_container === 'paneContainer1' ? 'opacity-50' : ''}`"
+            :pt="{ activeBar: 'tab-activebar-grad' }">
+            <draggable id="paneContainer1" class="min-h-[32px] w-full flex cursor-move" v-model="containerA"
+              item-key="id" group="shared" :sort="true" :move="checkMove" @end="onDragEnd">
+              <template #item="{ element }">
+                <Tab class="!px-2.5 !py-1 rounded" :key="element.value" :value="element.value">
+                  {{ element.name }}
+                </Tab>
+              </template>
+            </draggable>
+
+            <h6 v-if="containerA.length === 0"
+              class="w-full h-full absolute top-0 left-0 flex items-center justify-center border border-dashed border-slate-400 rounded dark:bg-[#0F182A] bg-white text-slate-400 text-sm text-center">
+              Drag and drop tabs
+            </h6>
+          </TabList>
+          <!-- @active_term_changed="active_term = $event" @active_layer_changed="active_layer = $event" -->
+          <TabPanelOption v-if="containerA.length > 0" v-model="active_tabA" :gephi_data="gephi_data"
+            :active_decoloumn="active_decoloumn" :active_node="active_node" :active_termlayers="active_termlayers"
+            :active_background="active_background" :terms="terms" :api="api" :mode="mode" :await_load="await_load" />
+        </Tabs>
+      </section>
+
+      <section :class="`flex relative overflow-hidden transition duration-300 ease-in-out ${containerB.length > 0 ? 'flex-[1_1_50%]' : ''
+        }`">
+        <Tabs class="flex-1 w-full tab-active-grad" scrollable :value="active_tabB" @update:value="onchangeTabB">
+          <TabList :class="`flex-shrink-0 ${dragged_to_container === 'paneContainer2' ? 'opacity-50' : ''}`">
+            <draggable id="paneContainer2" class="min-h-[32px] w-full flex cursor-move" v-model="containerB"
+              item-key="id" group="shared" :sort="true" :move="checkMove" @end="onDragEnd">
+              <template #item="{ element }">
+                <Tab class="!px-2.5 !py-1 rounded" :key="element.value" :value="element.value">
+                  {{ element.name }}
+                </Tab>
+              </template>
+            </draggable>
+
+            <h6 v-if="containerB.length === 0"
+              class="w-full h-full absolute top-0 left-0 flex items-center justify-center border border-dashed border-slate-400 rounded dark:bg-[#0F182A] bg-white text-slate-400 text-sm text-center">
+              Drag and drop tabs
+            </h6>
+          </TabList>
+          <!-- @active_term_changed="active_term = $event" @active_layer_changed="active_layer = $event" -->
+          <TabPanelOption v-if="containerB.length > 0" v-model="active_tabB" :gephi_data="gephi_data"
+            :active_decoloumn="active_decoloumn" :active_node="active_node" :active_termlayers="active_termlayers"
+            :active_background="active_background" :terms="terms" :api="api" :mode="mode" :await_load="await_load" />
+        </Tabs>
+      </section>
+    </template>
+  </Card>
+  <!-- <div id="vertical-pane">
     <div class="upper-block">
       <div class="tab-system">
         <ul>
@@ -102,43 +157,206 @@
         :active_background="active_background"
       ></CitationMenu>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
-import PathwayMenu from "@/components/enrichment/PathwayMenu.vue";
-import CitationMenu from "@/components/citation/CitationMenu.vue";
-import PathwayLayers from "@/components/pane/modules/layer/PathwayLayers.vue";
-import DifExpMenu from "@/components/pane/modules/difexp/DifExpMenu.vue";
+import draggable from "vuedraggable";
 
 export default {
   name: "VerticalPane",
   props: [
+    "mode",
     "gephi_data",
     "active_node",
     "active_background",
     "active_termlayers",
     "active_decoloumn",
   ],
-  components: {
-    PathwayMenu,
-    PathwayLayers,
-    DifExpMenu,
-    CitationMenu,
-  },
+  components: { draggable },
   data() {
     return {
       dcoloumns: this.$store.state.dcoloumns,
-      active_function_tab1: "set",
-      active_function_tab2: "list",
-      mode: "protein",
+      // active_function_tab1: "set",
+      // active_function_tab2: "list",
+      api: {
+        subgraph: "api/subgraph/enrichment",
+      },
+      terms: null,
+      terms_list: [],
+      await_load: false,
+      dragged_to_container: "",
+      active_tabA: 'list',
+      active_tabB: 'set',
+      containerA: [
+        { id: 1, name: "Pathways", value: "list" },
+        { id: 2, name: "Enrichment", value: "graph" },
+        { id: 3, name: "Heatmap", value: "heatmap" },
+        { id: 4, name: "Citation", value: "citation" },
+        { id: 5, name: "Subsets", value: "set" },
+        { id: 6, name: "Path layers", value: "layers" },
+        { id: 7, name: "Dif exp", value: "difexp" }
+      ],
+      containerB: [
+      ],
     };
   },
-  mounted() {},
+  watch: {
+    dcoloumns() {
+      // FIXME: remove diffexp based on dcoloumns()
+    },
+  },
+  mounted() {
+    var com = this;
+    // FIXME: Need condition according to mode?
+    com.generatePathways(
+      com.gephi_data.nodes[0].species,
+      com.gephi_data.nodes.map((node) => node.attributes["Name"])
+    );
+  },
+  methods: {
+    onchangeTabA(val) {
+      this.active_tabA = val
+    },
+    onchangeTabB(val) {
+      this.active_tab = val
+    },
+    setActiveTab(container, newIndex = 0) {
+      var com = this;
+      const items = container === "paneContainer1" ? com.containerA : com.containerB;
+
+      switch (container) {
+        case "paneContainer1":
+          com.active_tabA = items[newIndex].value;
+          break;
+        case "paneContainer2":
+          com.active_tabB = items[newIndex].value;
+          break;
+
+        default:
+          break;
+      }
+    },
+    onDragEnd(event) {
+      var com = this;
+      const { from, to } = event;
+      const items = from.id === "paneContainer1" ? com.containerA : com.containerB;
+      const active = from.id === "paneContainer1" ? com.active_tabA : com.active_tabB;
+
+      com.dragged_to_container = "";
+
+      // ### This works only if from and to container is different
+      if (from.id !== to.id) {
+        if (items.some((item) => item.value !== active)) com.setActiveTab(from.id); // ### Sets first item from the dragged container as active
+
+        com.setActiveTab(to.id, event.newIndex); // ### Sets Dragged item as active in new container
+      }
+
+      if (from.id === "paneContainer1" && com.containerA.length === 0) {
+        com.active_tabA = "";
+      } else if (from.id === "paneContainer2" && com.containerB.length === 0) {
+        com.active_tabB = "";
+      }
+    },
+    checkMove(evt) {
+      const { from, to } = evt;
+      this.dragged_to_container = from.id === to.id ? "" : to.id;
+    },
+    generatePathways(species, genes) {
+      var com = this;
+
+      //Adding proteins and species to formdata
+      var formData = new FormData();
+      formData.append("genes", genes);
+      formData.append("species_id", species);
+      formData.append(
+        "mapping",
+        JSON.stringify(com.gephi_data.settings["gene_alias_mapping"])
+      );
+
+      this.await_load = true;
+
+      //POST request for generating pathways
+      com.sourceToken = this.axios.CancelToken.source();
+      com.axios
+        .post(com.api.subgraph, formData, {
+          cancelToken: com.sourceToken.token,
+        })
+        .then((response) => {
+          com.terms = response.data.sort((t1, t2) => t1.fdr_rate - t2.fdr_rate);
+          if (com.terms_list.length == 0)
+            this.$store.commit("assign_current_enrichment_terms", com.terms);
+          com.terms_list.push(com.terms);
+          com.await_load = false;
+        });
+    },
+    // abort_enrichment() {
+    //   this.sourceToken.cancel("Request canceled");
+    //   this.await_load = false;
+    // },
+    // dragElement(elmnt, val) {
+    //   var pos1 = 0,
+    //     pos2 = 0,
+    //     pos3 = 0,
+    //     pos4 = 0;
+    //   if (document.getElementById(elmnt.id + "_header")) {
+    //     // if present, the header is where you move the DIV from:
+    //     document.getElementById(elmnt.id + "_header").onmousedown =
+    //       dragMouseDown;
+    //   } else {
+    //     // otherwise, move the DIV from anywhere inside the DIV:
+    //     elmnt.onmousedown = dragMouseDown;
+    //   }
+
+    //   function dragMouseDown(e) {
+    //     e = e || window.event;
+    //     e.preventDefault();
+    //     // get the mouse cursor position at startup:
+    //     pos3 = e.clientX;
+    //     pos4 = e.clientY;
+    //     document.onmouseup = closeDragElement;
+    //     // call a function whenever the cursor moves:
+    //     document.onmousemove = elementDrag;
+    //   }
+
+    //   function elementDrag(e) {
+    //     if (!val) return;
+    //     e = e || window.event;
+    //     e.preventDefault();
+    //     // calculate the new cursor position:
+    //     pos1 = pos3 - e.clientX;
+    //     pos2 = pos4 - e.clientY;
+    //     pos3 = e.clientX;
+    //     pos4 = e.clientY;
+    //     // set the element's new position:
+    //     elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    //     elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    //   }
+
+    //   function closeDragElement() {
+    //     // stop moving when mouse button is released:
+    //     document.onmouseup = null;
+    //     document.onmousemove = null;
+    //   }
+    // },
+    // resetElement(elmnt) {
+    //   // set the element's new position:
+    //   elmnt.style.top = null;
+    //   elmnt.style.left = null;
+    //   document.getElementById(elmnt.id + "_header").onmousedown = null;
+    // },
+  },
+  created() {
+    this.emitter.on("enrichTerms", (terms) => {
+      if (terms != null) this.terms = terms;
+      else this.terms = this.terms_list[0];
+      this.$store.commit("assign_current_enrichment_terms", this.terms);
+    });
+  },
 };
 </script>
 
-<style>
+<!-- <style>
 #vertical-pane {
   left: 1%;
   top: 1.5%;
@@ -239,4 +457,4 @@ ul {
   transform: translateX(-50%);
   border-bottom: 0.15vw solid #e6eaf5;
 }
-</style>
+</style> -->
