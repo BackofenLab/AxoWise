ce175125e761fe2a3b862fda6613320a9293f06f
anlisha-maharjan <anlishamaharjan@gmail.com>
2024-12-17 22:13:00 +0100

    NN-629 modify citation view to use new appbar component

--- frontend/src/components/appbar/SearchField.vue
+++ frontend/src/components/appbar/SearchField.vue
@@ -1,14 +1,26 @@
 <template>
-  <Button label="Find your node" icon="pi pi-search" severity="secondary" class="w-[220px] dark:!bg-[#020617]"
+  <Button label="Find your node" icon="pi pi-search" severity="secondary" class="w-[180px] dark:!bg-[#020617]"
     :pt="{ label: { class: 'mr-auto' } }" @click="show_search = !show_search" />
 
   <Dialog v-model:visible="show_search" position="topright" :minY="60" :minX="60" :closable="true" :pt="{
-    root: { class: 'w-[20rem] !mt-[60px] !ml-[60px] !mr-[7rem]' },
+    root: { class: 'w-[22rem] !mt-[60px] !ml-[60px] !mr-[7rem]' },
     headerActions: { class: '!hidden' },
     header: { class: '!p-3 order-1 cursor-move' },
     content: { class: 'order-2 !px-0' },
+    footer: {
+      class: 'order-3 !p-3 border-t border-slate-200 dark:border-slate-100/30',
+    },
   }">
-    <h6 v-if="filt_search.length === 0" class="text-center text-slate-300">No available options</h6>
+    <template #header>
+      <IconField class="w-full !mt-[10px]">
+        <InputIcon class="z-10 pi pi-search" />
+        <InputText v-model="search_raw" placeholder="Find your node" class="w-full"
+          v-on:keyup.enter="select_node(filt_search[0])" autofocus />
+      </IconField>
+    </template>
+
+    <h6 v-if="search_raw.length >= 2 && filt_search.length === 0" class="text-center text-slate-300">No available
+      options</h6>
 
     <ul class="divide-y border-slate-200 dark:divide-slate-100/10"
       v-if="search_raw.length >= 2 && filt_search.length > 0">
@@ -23,12 +35,14 @@
       </li>
     </ul>
 
-    <template #header>
-      <IconField class="w-full !mt-[10px]">
-        <InputIcon class="z-10 pi pi-search" />
-        <InputText v-model="search_raw" placeholder="Find your node" class="w-full"
-          v-on:keyup.enter="select_node(filt_search[0])" autofocus />
-      </IconField>
+    <template #footer>
+      <span class="flex items-center text-sm gap-1.5 text-slate-400">
+        <span
+          class="h-[20px] flex items-center font-semibold text-sm px-1 bg-slate-100 rounded text-slate-500 leading-none">
+          esc
+        </span>
+        to close
+      </span>
     </template>
   </Dialog>
 </template>
