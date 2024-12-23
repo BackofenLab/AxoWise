709a3922239948b46bb6a963dc9fe368b714a2ce
anlisha-maharjan <anlishamaharjan@gmail.com>
2024-12-23 21:11:35 +0100

    NN-637 modify citation summary UI

--- frontend/src/components/toolbar/modules/ProteinList.vue
+++ frontend/src/components/toolbar/modules/ProteinList.vue
@@ -1,8 +1,8 @@
 <template>
   <h6 class="mb-2 text-sm text-slate-300">
-    To find multiple nodes separate each node with comma (<span class="leading-[0] text-primary-400 text-4xl">,</span>)
+    To find multiple nodes separate each node in a new line.
   </h6>
-  <Textarea v-model="raw_text" rows="4" fluid autofocus placeholder="Search by gene..." />
+  <Textarea v-model="raw_text" rows="4" fluid autofocus placeholder="Search by gene..." class="text-center"/>
   <Button label="Apply & Create subset" severity="secondary" size="small" fluid type="button" class="mt-2.5 !rounded-lg"
     @click="highlight(raw_text)">
   </Button>
@@ -24,9 +24,7 @@ export default {
   methods: {
     highlight(proteins) {
       var com = this;
-      // This regex is not user friendly instead use ','
-      // const protein_names = new Set(proteins.toUpperCase().split("\n"));
-      const protein_names = new Set(proteins.toUpperCase().split(",").map(item => item.trim()));
+      const protein_names = new Set(proteins.toUpperCase().split("\n"));
       const subset = [];
       com.gephi_data.nodes.forEach((node) => {
         if (protein_names.has(node.attributes["Name"].toUpperCase())) {
