2c8a54bb4bbba1e4d88a21eb94974aa4647fde05
anlisha-maharjan <anlishamaharjan@gmail.com>
2025-01-02 21:54:29 +0100

    NN-641 remove unused routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import InputScreen from "../components/home/InputScreen.vue";
-import ImportScreen from "../components/home/ImportScreen.vue";
-import FileScreen from "../components/home/FileScreen.vue";
 import ProteinView from "../views/ProteinView.vue";
 import TermView from "../views/TermView.vue";
 import CitationView from "../views/CitationView.vue";
@@ -39,22 +36,7 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: CitationView,
     meta: { keepAlive: true }, // added meta to enable keep-alive
-  },
-  {
-    path: "/input",
-    name: "input",
-    component: InputScreen,
-  },
-  {
-    path: "/import",
-    name: "import",
-    component: ImportScreen,
-  },
-  {
-    path: "/file",
-    name: "file",
-    component: FileScreen,
-  },
+  }
 ];
 
 const router = createRouter({
