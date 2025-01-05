75b80b0604502dea141318d3170a466d6d86293f
anlisha-maharjan <anlishamaharjan@gmail.com>
2025-01-05 22:22:38 +0100

    NN-643 complete devalue pane

--- frontend/src/components/pane/modules/difexp/DEValuePane.vue
+++ frontend/src/components/pane/modules/difexp/DEValuePane.vue
@@ -1,57 +1,68 @@
 <template>
-  <div class="text" v-show="active_decoloumn !== null">
+  <div v-show="active_decoloumn !== null">
+    <header v-if="active_decoloumn !== null" class="flex flex-wrap items-center gap-2">
+      <span class="text-sm font-normal">{{ active_decoloumn }}</span>
+    </header>
+
+    <Tabs :value="active_section" @update:value="change_section">
+      <div
+        :class="`${active_section ? '!pt-2 !border-t !border-slate-700 !mt-2' : ''} px-2.5 -mx-2.5 max-h-[10rem] overflow-auto overflow-x-visible`">
+        <TabPanels class="!p-0">
+          <TabPanel value="statistics">
+            <h3 class="mb-3 text-sm font-medium">
+              Contained nodes
+            </h3>
+            <RegulatedProteins :active_decoloumn="active_decoloumn" :gephi_data="gephi_data"></RegulatedProteins>
+          </TabPanel>
+        </TabPanels>
+      </div>
+
+      <footer class="flex items-end !mt-2 !border-t !border-slate-600 py-2">
+        <TabList class="" :pt="{
+          tabList: { class: '!border-0 !gap-4' },
+          activeBar: { class: '!hidden' }
+        }">
+          <Tab value="statistics" class="!p-0 !border-0"><span
+              :class="`material-symbols-rounded !text-lg ${active_section == 'statistics' ? 'font-variation-ico-filled' : ''}`">tune</span>
+          </Tab>
+        </TabList>
+      </footer>
+    </Tabs>
+  </div>
+
+  <!-- <div class="text" v-show="active_decoloumn !== null">
     <div class="gene_attribute" v-if="active_decoloumn !== null">
       <div class="deval">{{ active_decoloumn }}</div>
     </div>
-    <div
-      :class="{
-        'tool-section': !tool_active,
-        'tool-section-active': tool_active,
-      }"
-    >
-      <div
-        id="informations"
-        class="subsection"
-        v-show="tool_active && active_section == 'information'"
-      >
+    <div :class="{
+      'tool-section': !tool_active,
+      'tool-section-active': tool_active,
+    }">
+      <div id="informations" class="subsection" v-show="tool_active && active_section == 'information'">
         <div class="subsection-header">
           <span>informations</span>
         </div>
         <div class="subsection-main colortype"></div>
       </div>
-      <div
-        id="network"
-        class="subsection"
-        v-show="tool_active && active_section == 'statistics'"
-      >
+      <div id="network" class="subsection" v-show="tool_active && active_section == 'statistics'">
         <div class="subsection-header">
           <span>contained nodes</span>
         </div>
         <div class="subsection-main colortype">
-          <RegulatedProteins
-            :active_decoloumn="active_decoloumn"
-            :gephi_data="gephi_data"
-          ></RegulatedProteins>
+          <RegulatedProteins :active_decoloumn="active_decoloumn" :gephi_data="gephi_data"></RegulatedProteins>
         </div>
       </div>
     </div>
     <div class="nodeattributes">
-      <img
-        class="icons"
-        src="@/assets/toolbar/menu-burger.png"
-        v-on:click="change_section('information')"
-      />
-      <img
-        class="icons"
-        src="@/assets/toolbar/settings-sliders.png"
-        v-on:click="change_section('statistics')"
-      />
+      <img class="icons" src="@/assets/toolbar/menu-burger.png" v-on:click="change_section('information')" />
+      <img class="icons" src="@/assets/toolbar/settings-sliders.png" v-on:click="change_section('statistics')" />
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
 import RegulatedProteins from "@/components/pane/modules/difexp/RegulatedProteins.vue";
+import { useToast } from "primevue/usetoast";
 export default {
   name: "DEValuePane",
   props: ["active_decoloumn", "gephi_data", "tool_active"],
@@ -66,6 +77,9 @@ export default {
         value: null,
       },
     };
+  },
+  mounted() {
+    this.toast = useToast();
   },
   watch: {
     active_decoloumn() {
@@ -105,12 +119,13 @@ export default {
       var textToCopy = [];
       for (var node of com.dvalueNodes) textToCopy.push(node.label);
       navigator.clipboard.writeText(textToCopy.join("\n"));
+      this.toast.add({ severity: 'success', detail: 'Message copied to clipboard.', life: 4000 });
     },
   },
 };
 </script>
 
-<style>
+<!-- <style>
 .gene_attribute .deval {
   width: 100%;
   margin-left: 0.3vw;
@@ -120,4 +135,4 @@ export default {
   text-overflow: ellipsis;
   text-align: center;
 }
-</style>
+</style> -->
--- frontend/src/components/pane/modules/difexp/RegulatedProteins.vue
+++ frontend/src/components/pane/modules/difexp/RegulatedProteins.vue
@@ -1,62 +1,114 @@
 <template>
-  <div id="de-connect" class="connect">
+  <ul class="list-none p-0 m-0 flex flex-col divide-y dark:divide-[#343b4c] mb-3">
+    <li class="flex items-center justify-between  py-1 dark:text-[#c3c3c3]" v-for="(key, entry, index) in selectionattr"
+      :key="index">
+      <strong class="text-sm font-normal capitalize dark:text-slate-400">{{ entry }}</strong>
+      <InputNumber :useGrouping="false" :min="key.min" :max="key.max" :step="key.step" v-model="key.value"
+        @value-change="select_proteins()" inputClass="w-28 h-5 !px-1.5 !text-xs text-center" />
+    </li>
+  </ul>
+
+  <Listbox v-if="filt_links?.length" optionLabel="" :options="filt_links" :pt="{
+    listContainer: { class: 'order-2' },
+    list: { class: '!p-0' },
+    emptyMessage: { class: '!flex !justify-center !items-center !text-sm !text-slate-500 dark:!text-slate-300' },
+    option: {
+      class:
+        '!px-0 !py-1 !text-slate-500 dark:!text-slate-300 leading-tight transition-all duration-300 ease-in-out',
+    },
+  }" listStyle="max-height:100%" class="h-full flex flex-col !p-0 !bg-transparent !border-0" :tabindex="0"
+    emptyMessage="No nodes available.">
+
+    <template #footer>
+      <header class="sticky -top-2 bg-[var(--card-bg)] items-center gap-2 z-[1] order-1">
+        <!-- sorting -->
+        <div
+          class="grid grid-cols-12 items-center gap-2 py-2 bg-[var(--card-bg)] shadow-[0_10px_30px_-18px_#34343D] dark:shadow-[0_10px_30px_-18px_#ffffff] z-[1]">
+
+          <a class="flex items-center justify-start col-span-4 gap-1 text-sm cursor-pointer" v-on:click="
+            sort_node = sort_node === 'asc' ? 'dsc' : 'asc';
+          sort_cluster = '';
+          sort_degree = '';
+          ">
+            Nodes
+            <span :class="`material-symbols-rounded text-sm cursor-pointer 
+            ${sort_node ? 'text-primary-500' : 'text-slate-600'}`">
+              {{ !sort_node ? "swap_vert" : sort_node === "asc" ? "south" : "north" }}
+            </span>
+          </a>
+
+          <a class="flex items-center justify-center col-span-4 gap-1 text-sm text-center cursor-pointer" v-on:click="
+            sort_cluster = sort_cluster === 'asc' ? 'dsc' : 'asc';
+          sort_node = '';
+          sort_degree = '';
+          ">
+            Cluster
+            <span :class="`material-symbols-rounded text-sm cursor-pointer 
+            ${sort_cluster ? 'text-primary-500' : 'text-slate-600'}`">
+              {{ !sort_cluster ? "swap_vert" : sort_cluster === "asc" ? "south" : "north" }}
+            </span>
+          </a>
+
+          <a class="flex items-center justify-center col-span-4 gap-1 text-sm text-center cursor-pointer" v-on:click="
+            sort_degree = sort_degree === 'asc' ? 'dsc' : 'asc';
+          sort_cluster = '';
+          sort_node = '';
+          ">
+            Degree
+            <span :class="`material-symbols-rounded text-sm cursor-pointer 
+            ${sort_degree ? 'text-primary-500' : 'text-slate-600'}`">
+              {{ !sort_degree ? "swap_vert" : sort_degree === "asc" ? "south" : "north" }}
+            </span>
+          </a>
+        </div>
+      </header>
+    </template>
+    <!-- options -->
+    <template #option="slotProps">
+      <div :class="`grid items-center w-full grid-cols-12 gap-2`">
+        <span class="col-span-4 text-xs">{{ slotProps.option?.attributes?.["Name"] }}</span>
+
+        <span class="col-span-4 text-xs text-center">{{ slotProps.option?.attributes?.["Modularity Class"] }}</span>
+
+        <span class="col-span-4 text-xs text-center">{{ slotProps.option?.attributes?.["Degree"] }}</span>
+      </div>
+    </template>
+  </Listbox>
+
+  <!-- <div id="de-connect" class="connect">
     <div class="selection-results">
       <table>
         <tbody>
-          <tr
-            v-for="(key, entry, index) in selectionattr"
-            :key="index"
-            class="option"
-          >
+          <tr v-for="(key, entry, index) in selectionattr" :key="index" class="option">
             <td>
               <div class="statistics-attr">
                 <a href="#">{{ entry }}</a>
               </div>
             </td>
             <td>
-              <input
-                class="statistics-val"
-                type="number"
-                v-bind:min="key.min"
-                v-bind:max="key.max"
-                v-bind:step="key.step"
-                v-model="key.value"
-                v-on:change="select_proteins()"
-              />
+              <input class="statistics-val" type="number" v-bind:min="key.min" v-bind:max="key.max"
+                v-bind:step="key.step" v-model="key.value" v-on:change="select_proteins()" />
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-
     <div class="sorting">
-      <a
-        class="node_filter"
-        v-on:click="
-          sort_node = sort_node === 'asc' ? 'dsc' : 'asc';
-          sort_cluster = '';
-          sort_degree = '';
-        "
-        >nodes</a
-      >
-      <a
-        class="cluster_filter"
-        v-on:click="
-          sort_cluster = sort_cluster === 'asc' ? 'dsc' : 'asc';
-          sort_node = '';
-          sort_degree = '';
-        "
-        >cluster</a
-      >
-      <a
-        class="degree_filter"
-        v-on:click="
-          sort_degree = sort_degree === 'asc' ? 'dsc' : 'asc';
-          sort_cluster = '';
-          sort_node = '';
-        "
-        >degree</a
-      >
+      <a class="node_filter" v-on:click="
+        sort_node = sort_node === 'asc' ? 'dsc' : 'asc';
+      sort_cluster = '';
+      sort_degree = '';
+      ">nodes</a>
+      <a class="cluster_filter" v-on:click="
+        sort_cluster = sort_cluster === 'asc' ? 'dsc' : 'asc';
+      sort_node = '';
+      sort_degree = '';
+      ">cluster</a>
+      <a class="degree_filter" v-on:click="
+        sort_degree = sort_degree === 'asc' ? 'dsc' : 'asc';
+      sort_cluster = '';
+      sort_node = '';
+      ">degree</a>
     </div>
 
     <div class="network-results" tabindex="0" @keydown="handleKeyDown">
@@ -71,7 +123,7 @@
             <td>
               <a class="statistics-val">{{
                 entry.attributes["Modularity Class"]
-              }}</a>
+                }}</a>
             </td>
             <td>
               <a class="statistics-val">{{ entry.attributes["Degree"] }}</a>
@@ -80,7 +132,7 @@
         </tbody>
       </table>
     </div>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -145,8 +197,8 @@ export default {
             ? 1
             : t1.attributes["Name"].toLowerCase() ===
               t2.attributes["Name"].toLowerCase()
-            ? 0
-            : -1;
+              ? 0
+              : -1;
         });
       } else if (com.sort_node == "dsc") {
         filtered.sort(function (t1, t2) {
@@ -155,8 +207,8 @@ export default {
             ? 1
             : t1.attributes["Name"].toLowerCase() ===
               t2.attributes["Name"].toLowerCase()
-            ? 0
-            : -1;
+              ? 0
+              : -1;
         });
       }
 
@@ -183,14 +235,14 @@ export default {
           (t1, t2) => t1.attributes["Degree"] - t2.attributes["Degree"]
         );
       }
-
-      return new Set(filtered);
+      return filtered;
+      // return new Set(filtered);
     },
   },
 };
 </script>
 
-<style>
+<!-- <style>
 #de-connect {
   width: 100%;
   height: 100%;
@@ -242,4 +294,4 @@ export default {
   overflow: hidden; /* Hide overflow content */
   text-overflow: ellipsis;
 }
-</style>
+</style> -->
